8949649dff79f927aa11fe75c7385b01705e5fab
zachreborn <zachreborn@users.noreply.github.com>
2023-03-28 19:57:29 +0000

    Continuous Integration - terraform fmt and terraform-docs

--- modules/aws/organizations/organization/outputs.tf
+++ modules/aws/organizations/organization/outputs.tf
@@ -3,35 +3,35 @@
 ############################################################
 output "accounts" {
   description = "List of organization accounts.All elements have these attributes: arn, email, id, name, status."
-  value = aws_organizations_organization.org.accounts
+  value       = aws_organizations_organization.org.accounts
 }
 
 output "master_account_arn" {
   description = "ARN of the master account"
-  value = aws_organizations_organization.org.master_account_arn
+  value       = aws_organizations_organization.org.master_account_arn
 }
 
 output "master_account_email" {
   description = "Email address of the master account"
-  value = aws_organizations_organization.org.master_account_email
+  value       = aws_organizations_organization.org.master_account_email
 }
 
 output "master_account_id" {
   description = "ID of the master account"
-  value = aws_organizations_organization.org.master_account_id
+  value       = aws_organizations_organization.org.master_account_id
 }
 
 output "arn" {
   description = "ARN of the organization"
-  value = aws_organizations_organization.org.arn
+  value       = aws_organizations_organization.org.arn
 }
 
 output "id" {
   description = "ID of the organization"
-  value = aws_organizations_organization.org.id
+  value       = aws_organizations_organization.org.id
 }
 
 output "roots" {
   description = "List of organization roots.All elements have these attributes: arn, id, name, policy_types."
-  value = aws_organizations_organization.org.roots
+  value       = aws_organizations_organization.org.roots
 }
