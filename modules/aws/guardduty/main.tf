86e898f76c7ba6ac2a1974b6655fc66c6d174d92
Zachary Hill <zhill@octo.codes>
2023-03-29 17:46:59 -0400

    added provider for the security account

--- modules/aws/guardduty/main.tf
+++ modules/aws/guardduty/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      version               = ">= 4.0.0"
+      configuration_aliases = [aws.organization_master_account, aws.organization_security_account]
+    }
+  }
+}
+
 resource "aws_guardduty_detector" "this" {
   enable                       = var.enable
   finding_publishing_frequency = var.finding_publishing_frequency
@@ -9,6 +20,7 @@ resource "aws_guardduty_organization_admin_account" "this" {
 }
 
 resource "aws_guardduty_organization_configuration" "this" {
+  provider    = aws.organization_security_account
   count       = var.enable_organization ? 1 : 0
   auto_enable = var.auto_enable
   detector_id = aws_guardduty_detector.this.id
