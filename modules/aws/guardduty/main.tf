02e3b972a71c66368e9f5e1ade60363788b445a5
Zachary Hill <zhill@octo.codes>
2023-03-29 11:31:22 -0400

    adding it back in

--- modules/aws/guardduty/main.tf
+++ modules/aws/guardduty/main.tf
@@ -8,9 +8,8 @@ resource "aws_guardduty_organization_admin_account" "this" {
   admin_account_id = var.admin_account_id
 }
 
-/* resource "aws_guardduty_organization_configuration" "this" {
+resource "aws_guardduty_organization_configuration" "this" {
   count       = var.enable_organization ? 1 : 0
   auto_enable = var.auto_enable
   detector_id = aws_guardduty_detector.this.id
 }
- */
