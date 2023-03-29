40279d81c66e5d08db993e5cd35474771eb189e9
Zachary Hill <zhill@octo.codes>
2023-03-29 18:03:18 -0400

    testing with multiple providers

--- modules/aws/guardduty/main.tf
+++ modules/aws/guardduty/main.tf
@@ -9,6 +9,7 @@ terraform {
   }
 }
 
+# GuardDuty Detector
 resource "aws_guardduty_detector" "this" {
   enable                       = var.enable
   finding_publishing_frequency = var.finding_publishing_frequency
