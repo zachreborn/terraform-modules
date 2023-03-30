6a013e8b65569d8eb453b2a637a4d327b38c64ac
Zachary Hill <zhill@zacharyhill.co>
2023-03-30 17:12:02 -0400

    removing the broken provider configuration for now

--- modules/aws/guardduty/main.tf
+++ modules/aws/guardduty/main.tf
@@ -4,7 +4,7 @@ terraform {
     aws = {
       source                = "hashicorp/aws"
       version               = ">= 4.0.0"
-      configuration_aliases = [aws.organization_master_account, aws.organization_security_account]
+      # configuration_aliases = [aws.organization_master_account, aws.organization_security_account]
     }
   }
 }
@@ -16,14 +16,14 @@ terraform {
 } */
 
 resource "aws_guardduty_organization_admin_account" "this" {
-  provider         = aws.organization_master_account
+  # provider         = aws.organization_master_account
   count            = var.enable_organization ? 1 : 0
   admin_account_id = var.admin_account_id
 }
 
-resource "aws_guardduty_organization_configuration" "this" {
-  provider    = aws.organization_security_account
-  count       = var.enable_organization ? 1 : 0
-  auto_enable = var.auto_enable
-  detector_id = aws_guardduty_detector.this.id
-}
+# resource "aws_guardduty_organization_configuration" "this" {
+#   provider    = aws.organization_security_account
+#   count       = var.enable_organization ? 1 : 0
+#   auto_enable = var.auto_enable
+#   detector_id = aws_guardduty_detector.this.id
+# }
