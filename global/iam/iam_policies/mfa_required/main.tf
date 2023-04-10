61c9dd45a5283542f4fc14ae25d392ad28670063
Zachary Hill <zhill@zacharyhill.co>
2023-04-10 16:37:46 -0400

    added terraform requirements

--- global/iam/iam_policies/mfa_required/main.tf
+++ global/iam/iam_policies/mfa_required/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
 resource "aws_iam_policy" "mfa_required" {
   name        = var.mfa_required_policy_name
   description = "Allows users to manage their own MFA settings"
