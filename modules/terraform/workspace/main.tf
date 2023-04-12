15d517cd421ad1740201bab3ee702c5ad1bf2ecc
Zachary Hill <zhill@zacharyhill.co>
2023-04-12 15:38:14 -0400

    bugfix for the dynamic credentials

--- modules/terraform/workspace/main.tf
+++ modules/terraform/workspace/main.tf
@@ -93,7 +93,7 @@ resource "aws_iam_openid_connect_provider" "terraform_cloud" {
     var.terraform_cloud_aws_audience
   ]
   thumbprint_list = [
-    data.tls_certificate.terraform_cloud_certificate.sha1_fingerprint
+    data.tls_certificate.terraform_cloud_certificate[0].sha1_fingerprint
   ]
 }
 
