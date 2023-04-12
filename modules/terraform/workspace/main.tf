c7c147a9f7998872029f607f7f6db7ad758dbf88
Zachary Hill <zhill@zacharyhill.co>
2023-04-12 12:58:06 -0400

    added the resources and variables to support the AWS OIDC configuration

--- modules/terraform/workspace/main.tf
+++ modules/terraform/workspace/main.tf
@@ -12,7 +12,7 @@ terraform {
 # Data Sources
 ##############################
 data "tls_certificate" "terraform_cloud_certificate" {
-  url = "https://${var.tfc_hostname}"
+  url = "https://${var.terraform_cloud_hostname}"
 }
 
 ##############################
@@ -72,7 +72,7 @@ resource "tfe_variable" "tfc_aws_provider_auth" {
   value        = "true"
 }
 
-resource "tfe_variable" "tfc_aws_role_arn" {
+resource "tfe_variable" "tfc_aws_run_role_arn" {
   count        = var.enable_dynamic_credentials ? 1 : 0
   workspace_id = tfe_workspace.this.id
   category     = "env"
@@ -81,3 +81,49 @@ resource "tfe_variable" "tfc_aws_role_arn" {
   value        = var.dynamic_role_arn
 }
 
+##############################
+# AWS Identity Provider
+##############################
+# Used if enable_aws is true
+resource "aws_iam_openid_connect_provider" "terraform_cloud" {
+  count           = var.enable_aws ? 1 : 0
+  url             = data.tls_certificate.terraform_cloud_certificate.url
+  tags            = var.tags
+  client_id_list  = [
+    var.terraform_cloud_aws_audience
+  ]
+  thumbprint_list = [
+    data.tls_certificate.terraform_cloud_certificate.sha1_fingerprint
+  ]
+}
+
+resource "aws_iam_role" "terraform_cloud" {
+  count              = var.enable_aws ? 1 : 0
+  name               = var.iam_role_name
+  assume_role_policy = jsonencode({
+    "Version" =  "2012-10-17",
+    "Statement" = [
+      {
+        "Effect" = "Allow",
+        "Principal" = {
+          "Federated" = "${aws_iam_openid_connect_provider.terraform_cloud.arn}"
+        },
+        "Action" = "sts:AssumeRoleWithWebIdentity",
+        "Condition" = {
+          "StringEquals" = {
+            "${var.terraform_cloud_hostname}:aud": "${var.terraform_cloud_aws_audience}"
+          },
+          "StringLike" = {
+            "${var.terraform_cloud_hostname}:sub": "organization:${var.organization}:project:${var.terraform_cloud_project_name}:workspace:${var.name}:run_phase:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_cloud" {
+  count      = var.enable_aws ? 1 : 0
+  role       = aws_iam_role.terraform_cloud.name
+  policy_arn = var.terraform_role_policy_arn
+}
