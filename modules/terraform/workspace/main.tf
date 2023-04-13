afbd1cd60e78bbd5e9602339582aaeebd1e2d609
Zachary Hill <zhill@zacharyhill.co>
2023-04-13 12:29:34 -0400

    moved bootstrapping code to a new module

--- modules/terraform/workspace/main.tf
+++ modules/terraform/workspace/main.tf
@@ -9,13 +9,6 @@ terraform {
 }
 
 ##############################
-# Data Sources
-##############################
-/* data "tls_certificate" "terraform_cloud_certificate" {
-  url = "https://${var.terraform_cloud_hostname}"
-} */
-
-##############################
 # Terraform Workspace
 ##############################
 
@@ -80,52 +73,3 @@ resource "tfe_variable" "tfc_aws_run_role_arn" {
   key          = "TFC_AWS_RUN_ROLE_ARN"
   value        = var.dynamic_role_arn
 }
-
-##############################
-# AWS Identity Provider
-##############################
-# Used if enable_aws is true
-/* Commented out due to strategy change with bootstrapping the provider
-resource "aws_iam_openid_connect_provider" "terraform_cloud" {
-  count = var.enable_aws ? 1 : 0
-  url   = data.tls_certificate.terraform_cloud_certificate.url
-  tags  = var.tags
-  client_id_list = [
-    var.terraform_cloud_aws_audience
-  ]
-  thumbprint_list = [
-    data.tls_certificate.terraform_cloud_certificate.certificates[0].sha1_fingerprint
-  ]
-}
-
-resource "aws_iam_role" "terraform_cloud" {
-  count = var.enable_aws ? 1 : 0
-  name  = var.iam_role_name
-  assume_role_policy = jsonencode({
-    "Version" = "2012-10-17",
-    "Statement" = [
-      {
-        "Effect" = "Allow",
-        "Principal" = {
-          "Federated" = "${aws_iam_openid_connect_provider.terraform_cloud[0].arn}"
-        },
-        "Action" = "sts:AssumeRoleWithWebIdentity",
-        "Condition" = {
-          "StringEquals" = {
-            "${var.terraform_cloud_hostname}:aud" : "${var.terraform_cloud_aws_audience}"
-          },
-          "StringLike" = {
-            "${var.terraform_cloud_hostname}:sub" : "organization:${var.organization}:project:${var.terraform_cloud_project_name}:workspace:${var.name}:run_phase:*"
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "terraform_cloud" {
-  count      = var.enable_aws ? 1 : 0
-  role       = aws_iam_role.terraform_cloud[0].name
-  policy_arn = var.terraform_role_policy_arn
-}
- */
--- modules/terraform/workspace/variables.tf
+++ modules/terraform/workspace/variables.tf
@@ -1,13 +1,4 @@
 ##############################
-# Data Source Variables
-##############################
-/* variable "terraform_cloud_hostname" {
-  type        = string
-  description = "The hostname of the Terraform Cloud or Terraform Enterprise environment you'd like to use with the identity provider"
-  default     = "app.terraform.io"
-} */
-
-##############################
 # Terraform Workspace Variables
 ##############################
 
@@ -209,50 +200,3 @@ variable "dynamic_role_arn" {
   description = "(Optional) The ARN of the IAM role to assume when generating dynamic credentials for this workspace. This is only required if enable_dynamic_credentials is true."
   default     = null
 }
-
-##############################
-# AWS Identity Provider Variables
-##############################
-/* Commented out due to strategy with bootstrapping the provider
-variable "terraform_cloud_aws_audience" {
-  type        = string
-  default     = "aws.workload.identity"
-  description = "The audience value to use in the terraform run identity tokens"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "(Optional) A map of tags to assign to the workspace."
-  default = {
-    terraform = "true"
-  }
-}
-
-variable "enable_aws" {
-  type        = bool
-  description = "(Optional) Whether to enable AWS as an identity provider for this workspace. Defaults to false."
-  default     = false
-  validation {
-    condition     = can(regex("true|false", var.enable_aws))
-    error_message = "enable_aws must be true or false."
-  }
-}
-
-variable "iam_role_name" {
-  type        = string
-  description = "(Optional) The name of the IAM role to assume when generating dynamic credentials for this workspace. This is only required if enable_aws is true."
-  default     = "terraform_cloud"
-}
-
-variable "terraform_cloud_project_name" {
-  type        = string
-  description = "(Optional) The name of the Terraform Cloud project which the workspace is in. This is only required if enable_aws is true."
-  default     = "Default Project"
-}
-
-variable "terraform_role_policy_arn" {
-  type        = string
-  description = "AWS IAM AdministratorAccess policy arn"
-  default     = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
- */
