5896a97a1d7dee9f370fa3e4874354748892a2f8
Zachary Hill <zhill@zacharyhill.co>
2023-04-14 11:25:48 -0400

    added the outputs

---
+++ modules/azuread/group/outputs.tf
@@ -0,0 +1,14 @@
+output "mail" {
+    description = "The SMTP address for the group."
+    value       = azuread_group.this.mail
+}
+
+output "object_id" {
+  description = "The object ID of the Azure AD group."
+  value       = azuread_group.this.object_id
+}
+
+output "proxy_addresses" {
+    description = "The proxy addresses for the group."
+    value       = azuread_group.this.proxy_addresses
+}
