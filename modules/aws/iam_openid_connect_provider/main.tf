a079f6f904ab1640f626e7d34a404a71db60d8b2
Zachary Hill <zhill@zacharyhill.co>
2023-06-12 18:22:34 -0400

    initial commit

---
+++ modules/aws/iam_openid_connect_provider/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+    client_id_list  = var.client_id_list
+    tags            = var.tags
+    thumbprint_list = var.thumbprint_list
+    url             = var.url
+}
