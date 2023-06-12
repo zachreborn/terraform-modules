c9c4f70ce2dace821f26ad074f94339ca4efe816
zachreborn <zachreborn@users.noreply.github.com>
2023-06-12 16:27:04 +0000

    Continuous Integration - terraform fmt and terraform-docs

--- modules/aws/iam_password_policy/README.md
+++ modules/aws/iam_password_policy/README.md
@@ -84,7 +84,9 @@ No requirements.
 
 ## Providers
 
-No providers.
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | n/a |
 
 ## Modules
 
@@ -92,11 +94,23 @@ No modules.
 
 ## Resources
 
-No resources.
+| Name | Type |
+|------|------|
+| [aws_iam_account_password_policy.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_account_password_policy) | resource |
 
 ## Inputs
 
-No inputs.
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_allow_users_to_change_password"></a> [allow\_users\_to\_change\_password](#input\_allow\_users\_to\_change\_password) | (Optional) Whether to allow users to change their own password | `bool` | `true` | no |
+| <a name="input_hard_expiry"></a> [hard\_expiry](#input\_hard\_expiry) | (Optional) Whether users are prevented from setting a new password after their password has expired (i.e. require administrator reset) | `bool` | `false` | no |
+| <a name="input_max_password_age"></a> [max\_password\_age](#input\_max\_password\_age) | (Optional) The number of days that an user password is valid. | `number` | `null` | no |
+| <a name="input_minimum_password_length"></a> [minimum\_password\_length](#input\_minimum\_password\_length) | (Optional) Minimum length to require for user passwords. Recommended to be set to >= 14. | `number` | `14` | no |
+| <a name="input_password_reuse_prevention"></a> [password\_reuse\_prevention](#input\_password\_reuse\_prevention) | (Optional) The number of previous passwords that users are prevented from reusing. | `number` | `24` | no |
+| <a name="input_require_lowercase_characters"></a> [require\_lowercase\_characters](#input\_require\_lowercase\_characters) | (Optional) Whether to require lowercase characters for user passwords. | `bool` | `true` | no |
+| <a name="input_require_numbers"></a> [require\_numbers](#input\_require\_numbers) | (Optional) Whether to require numbers for user passwords. | `bool` | `true` | no |
+| <a name="input_require_symbols"></a> [require\_symbols](#input\_require\_symbols) | (Optional) Whether to require symbols for user passwords. | `bool` | `true` | no |
+| <a name="input_require_uppercase_characters"></a> [require\_uppercase\_characters](#input\_require\_uppercase\_characters) | (Optional) Whether to require uppercase characters for user passwords. | `bool` | `true` | no |
 
 ## Outputs
 
--- modules/aws/iam_password_policy/variables.tf
+++ modules/aws/iam_password_policy/variables.tf
@@ -3,7 +3,7 @@ variable "allow_users_to_change_password" {
   description = "(Optional) Whether to allow users to change their own password"
   default     = true
   validation {
-    condition    = can(regex("^true|false$", var.allow_users_to_change_password))
+    condition     = can(regex("^true|false$", var.allow_users_to_change_password))
     error_message = "allow_users_to_change_password must be true or false"
   }
 }
@@ -13,7 +13,7 @@ variable "hard_expiry" {
   description = "(Optional) Whether users are prevented from setting a new password after their password has expired (i.e. require administrator reset)"
   default     = false
   validation {
-    condition    = can(regex("^true|false$", var.hard_expiry))
+    condition     = can(regex("^true|false$", var.hard_expiry))
     error_message = "hard_expiry must be true or false"
   }
 }
@@ -23,7 +23,7 @@ variable "max_password_age" {
   description = "(Optional) The number of days that an user password is valid."
   default     = null
   validation {
-    condition    = var.max_password_age == null ? true : (var.max_password_age > 0 && var.max_password_age < 1095)
+    condition     = var.max_password_age == null ? true : (var.max_password_age > 0 && var.max_password_age < 1095)
     error_message = "max_password_age must be null, or between 1 and 1095"
   }
 }
@@ -33,7 +33,7 @@ variable "minimum_password_length" {
   description = "(Optional) Minimum length to require for user passwords. Recommended to be set to >= 14."
   default     = 14
   validation {
-    condition    = var.minimum_password_length > 0 && var.minimum_password_length < 128
+    condition     = var.minimum_password_length > 0 && var.minimum_password_length < 128
     error_message = "minimum_password_length must be between 1 and 128"
   }
 }
@@ -43,7 +43,7 @@ variable "password_reuse_prevention" {
   description = "(Optional) The number of previous passwords that users are prevented from reusing."
   default     = 24
   validation {
-    condition    = var.password_reuse_prevention == null ? true : (var.password_reuse_prevention > 0 && var.password_reuse_prevention < 25)
+    condition     = var.password_reuse_prevention == null ? true : (var.password_reuse_prevention > 0 && var.password_reuse_prevention < 25)
     error_message = "password_reuse_prevention must be null, or between 1 and 24"
   }
 }
@@ -53,7 +53,7 @@ variable "require_lowercase_characters" {
   description = "(Optional) Whether to require lowercase characters for user passwords."
   default     = true
   validation {
-    condition    = can(regex("^true|false$", var.require_lowercase_characters))
+    condition     = can(regex("^true|false$", var.require_lowercase_characters))
     error_message = "require_lowercase_characters must be true or false"
   }
 }
@@ -63,7 +63,7 @@ variable "require_numbers" {
   description = "(Optional) Whether to require numbers for user passwords."
   default     = true
   validation {
-    condition    = can(regex("^true|false$", var.require_numbers))
+    condition     = can(regex("^true|false$", var.require_numbers))
     error_message = "require_numbers must be true or false"
   }
 }
@@ -73,7 +73,7 @@ variable "require_symbols" {
   description = "(Optional) Whether to require symbols for user passwords."
   default     = true
   validation {
-    condition    = can(regex("^true|false$", var.require_symbols))
+    condition     = can(regex("^true|false$", var.require_symbols))
     error_message = "require_symbols must be true or false"
   }
 }
@@ -83,7 +83,7 @@ variable "require_uppercase_characters" {
   description = "(Optional) Whether to require uppercase characters for user passwords."
   default     = true
   validation {
-    condition    = can(regex("^true|false$", var.require_uppercase_characters))
+    condition     = can(regex("^true|false$", var.require_uppercase_characters))
     error_message = "require_uppercase_characters must be true or false"
   }
 }
