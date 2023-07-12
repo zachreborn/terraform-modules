70216938981e9b2d518eae6a97c6a6db6cf7fe64
Zachary Hill <zhill@zacharyhill.co>
2023-07-12 19:20:45 -0400

    initial module build

---
+++ modules/aws/efs/outputs.tf
@@ -0,0 +1,14 @@
+output "arn" {
+  description = "The ARN of the file system."
+  value       = aws_efs_file_system.this.arn
+}
+
+output "dns_name" {
+  description = "The DNS name of the file system."
+  value       = aws_efs_file_system.this.dns_name
+}
+
+output "id" {
+  description = "The ID that identifies the file system (e.g. fs-ccfc0d65)."
+  value       = aws_efs_file_system.this.id
+}
