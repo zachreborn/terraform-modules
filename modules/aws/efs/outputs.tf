2794e6b14cc779fc0c14b1bf777ead706e9e1718
zachreborn <zachreborn@users.noreply.github.com>
2023-07-13 18:15:06 +0000

    Continuous Integration - terraform fmt and terraform-docs

--- modules/aws/efs/outputs.tf
+++ modules/aws/efs/outputs.tf
@@ -15,7 +15,7 @@ output "id" {
 
 output "mount_target_dns_names" {
   description = "List of DNS names for the EFS File System."
-  value       = {
+  value = {
     for mount_target in aws_efs_mount_target.this : mount_target.id => mount_target.dns_name
   }
 }
