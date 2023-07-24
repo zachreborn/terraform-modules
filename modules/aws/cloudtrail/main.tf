80fea42a33ba7c231620b087d884e555494a0051
Zachary Hill <zhill@zacharyhill.co>
2023-07-24 12:24:39 -0400

    updated to just the org putobject

--- modules/aws/cloudtrail/main.tf
+++ modules/aws/cloudtrail/main.tf
@@ -267,25 +267,6 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
         }
       },
       {
-        "Sid"    = "AWSCloudTrailWrite",
-        "Effect" = "Allow",
-        "Principal" = {
-          "Service" = "cloudtrail.amazonaws.com"
-        },
-        "Action" = [
-          "s3:PutObject",
-          "s3:PutObjectAcl",
-          "s3:GetObject"
-        ],
-        "Resource" = "${aws_s3_bucket.cloudtrail_s3_bucket.arn}/*",
-        "Condition" = {
-          "StringEquals" = {
-            "AWS:SourceArn" = "arn:aws:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/${var.name}",
-            "s3:x-amz-acl"  = "bucket-owner-full-control"
-          }
-        }
-      },
-      {
         "Sid"    = "AWSCloudTrailOrganizationWrite",
         "Effect" = "Allow",
         "Principal" = {
