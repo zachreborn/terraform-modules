7da45463e762bc7aa34dff333ace3d667e9430c7
Zachary Hill <zhill@zacharyhill.co>
2023-08-19 19:29:36 -0400

    updates to variable extra_params

--- modules/aws/route53/registered_domain/variables.tf
+++ modules/aws/route53/registered_domain/variables.tf
@@ -65,7 +65,7 @@ variable "registrant_contact" {
     contact_type      = string
     country_code      = string
     email             = string
-    extra_params      = map
+    extra_params      = map(any)
     fax               = string
     first_name        = string
     last_name         = string
