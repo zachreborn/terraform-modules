e1d356af7ce607054c3f65d02f5934b2f8204ab7
Zachary Hill <zhill@zacharyhill.co>
2023-08-19 20:35:52 -0400

    enforce uppercase on contact_type, contry_code, and state

--- modules/aws/route53/registered_domain/main.tf
+++ modules/aws/route53/registered_domain/main.tf
@@ -30,8 +30,8 @@ resource "aws_route53domains_registered_domain" "this" {
     address_line_1    = var.admin_contact.address_line_1
     address_line_2    = var.admin_contact.address_line_2
     city              = var.admin_contact.city
-    contact_type      = var.admin_contact.contact_type
-    country_code      = var.admin_contact.country_code
+    contact_type      = upper(var.admin_contact.contact_type)
+    country_code      = upper(var.admin_contact.country_code)
     email             = var.admin_contact.email
     extra_params      = var.admin_contact.extra_params
     fax               = var.admin_contact.fax
@@ -39,7 +39,7 @@ resource "aws_route53domains_registered_domain" "this" {
     last_name         = var.admin_contact.last_name
     organization_name = var.admin_contact.organization_name
     phone_number      = var.admin_contact.phone_number
-    state             = var.admin_contact.state
+    state             = upper(var.admin_contact.state)
     zip_code          = var.admin_contact.zip_code
   }
 
@@ -47,8 +47,8 @@ resource "aws_route53domains_registered_domain" "this" {
     address_line_1    = var.registrant_contact.address_line_1
     address_line_2    = var.registrant_contact.address_line_2
     city              = var.registrant_contact.city
-    contact_type      = var.registrant_contact.contact_type
-    country_code      = var.registrant_contact.country_code
+    contact_type      = upper(var.registrant_contact.contact_type)
+    country_code      = upper(var.registrant_contact.country_code)
     email             = var.registrant_contact.email
     extra_params      = var.registrant_contact.extra_params
     fax               = var.registrant_contact.fax
@@ -56,7 +56,7 @@ resource "aws_route53domains_registered_domain" "this" {
     last_name         = var.registrant_contact.last_name
     organization_name = var.registrant_contact.organization_name
     phone_number      = var.registrant_contact.phone_number
-    state             = var.registrant_contact.state
+    state             = upper(var.registrant_contact.state)
     zip_code          = var.registrant_contact.zip_code
   }
 
@@ -64,8 +64,8 @@ resource "aws_route53domains_registered_domain" "this" {
     address_line_1    = var.tech_contact.address_line_1
     address_line_2    = var.tech_contact.address_line_2
     city              = var.tech_contact.city
-    contact_type      = var.tech_contact.contact_type
-    country_code      = var.tech_contact.country_code
+    contact_type      = upper(var.tech_contact.contact_type)
+    country_code      = upper(var.tech_contact.country_code)
     email             = var.tech_contact.email
     extra_params      = var.tech_contact.extra_params
     fax               = var.tech_contact.fax
@@ -73,7 +73,7 @@ resource "aws_route53domains_registered_domain" "this" {
     last_name         = var.tech_contact.last_name
     organization_name = var.tech_contact.organization_name
     phone_number      = var.tech_contact.phone_number
-    state             = var.tech_contact.state
+    state             = upper(var.tech_contact.state)
     zip_code          = var.tech_contact.zip_code
   }
 
