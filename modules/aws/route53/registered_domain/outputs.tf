0b0adf285901f7fb4821e8b6d490a60a23c93b9b
Zachary Hill <zhill@zacharyhill.co>
2023-08-19 20:41:02 -0400

    added outputs

--- modules/aws/route53/registered_domain/outputs.tf
+++ modules/aws/route53/registered_domain/outputs.tf
@@ -1,3 +1,34 @@
 ########################################
 # Route 53 Registered Domains
 ########################################
+output "creation_date" {
+  description = "The creation date of the domain."
+  value       = {
+    for domain in aws_route53domains_registered_domain.this :
+    domain.domain_name => domain.creation_date
+  }
+}
+
+output "expiration_date" {
+  description = "The expiration date of the domain."
+  value       = {
+    for domain in aws_route53domains_registered_domain.this :
+    domain.domain_name => domain.expiration_date
+  }
+}
+
+output "updated_date" {
+    description = "The last updated date of the domain."
+    value       = {
+        for domain in aws_route53domains_registered_domain.this :
+        domain.domain_name => domain.updated_date
+    }
+}
+
+output "whois_server" {
+    description = "The whois server of the domain."
+    value       = {
+        for domain in aws_route53domains_registered_domain.this :
+        domain.domain_name => domain.whois_server
+    }
+}
