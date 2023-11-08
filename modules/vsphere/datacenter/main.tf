ad764c9240b2c30ec1f0ed9390eeb5428104a363
Zachary Hill <zhill@octo.ventures>
2023-11-08 15:40:41 -0500

    added provider requirements

--- modules/vsphere/datacenter/main.tf
+++ modules/vsphere/datacenter/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/vsphere"
+      version = ">= 2.5.0"
+    }
+  }
+}
+
 resource "vsphere_datacenter" "this" {
   folder = var.folder
   name   = var.name
--- modules/vsphere/folder/main.tf
+++ modules/vsphere/folder/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/vsphere"
+      version = ">= 2.5.0"
+    }
+  }
+}
+
+
 resource "vsphere_folder" "this" {
   custom_attributes = var.custom_attributes
   datacenter_id     = var.datacenter_id
