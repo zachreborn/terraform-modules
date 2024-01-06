d6f90356cc718aed4331925e875b5fd39f2b1cf8
Zachary Hill <zhill@zacharyhill.co>
2024-01-06 19:44:49 -0400

    added security group and acm certificate

--- modules/aws/vpn_client/main.tf
+++ modules/aws/vpn_client/main.tf
@@ -25,8 +25,39 @@ terraform {
 ###########################
 
 # TODO - Create a security group resource for the VPN client
+resource "aws_security_group" "this" {
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = var.ingress_description
+    from_port   = var.ingress_from_port
+    to_port     = var.ingress_to_port
+    protocol    = var.ingress_protocol
+    cidr_blocks = var.ingress_cidr_blocks
+  }
+
+  egress {
+    description = var.egress_description
+    from_port   = var.egress_from_port
+    to_port     = var.egress_to_port
+    protocol    = var.egress_protocol
+    cidr_blocks = var.egress_cidr_blocks
+  }
+
+  tags = var.tags
+}
 
 # TODO - Create a ACM certificate to apply to the client vpn endpoint
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.domain_name
+  validation_method = var.validation_method
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 resource "aws_ec2_client_vpn_endpoint" "this" {
   client_cidr_block      = var.client_cidr_block
