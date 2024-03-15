cba18ceed5e8c5ea01a929e1b9a93f70f1c32fe7
Zachary Hill <zhill@octo.ventures>
2024-03-14 22:08:17 -0400

    correcting group outputs based on for_each

--- modules/aws/iam/group/outputs.tf
+++ modules/aws/iam/group/outputs.tf
@@ -1,19 +1,20 @@
 output "arn" {
-  description = "The ARN assigned by AWS for this IAM group."
-  value       = aws_iam_group.this.arn
+  description = "A map of ARNs assigned by AWS for the IAM groups."
+  value       = {
+    for group in aws_iam_group.this : group.name => group.arn
+  }
 }
 
 output "id" {
-  description = "The ID of the IAM group."
-  value       = aws_iam_group.this.id
-}
-
-output "name" {
-  description = "The name of the IAM group."
-  value       = aws_iam_group.this.name
+  description = "A map of IDs of the IAM groups."
+  value       = {
+    for group in aws_iam_group.this : group.name => group.id
+  }
 }
 
 output "path" {
-  description = "The path of the IAM group in AWS."
-  value       = aws_iam_group.this.path
+  description = "A map of the paths for each IAM group."
+  value       = {
+    for group in aws_iam_group.this : group.name => group.path
+  }
 }
