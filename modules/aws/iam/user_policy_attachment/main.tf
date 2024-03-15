148b8c8ccfeb0cc705a96eefa2c3370e52106dad
Zachary Hill <zhill@octo.ventures>
2024-03-14 21:45:26 -0400

    comment header

--- modules/aws/iam/user_policy_attachment/main.tf
+++ modules/aws/iam/user_policy_attachment/main.tf
@@ -12,6 +12,10 @@ terraform {
   }
 }
 
+##############################
+# Policy Attachment Configuration
+##############################
+
 resource "aws_iam_user_policy_attachment" "this" {
   policy_arn = var.policy_arn
   user       = var.user
