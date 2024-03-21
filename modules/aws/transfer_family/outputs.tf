f63b6593ae4c656f97f5c4edce0a9d84d94fbcd1
Zachary Hill <zhill@octo.ventures>
2024-03-20 21:39:57 -0400

    added outputs for the transfer_family_server

--- modules/aws/transfer_family/outputs.tf
+++ modules/aws/transfer_family/outputs.tf
@@ -2,3 +2,22 @@
 # Resource Outputs
 ###########################
 
+output "server_arn" {
+  description = "The ARN of the transfer family server"
+  value       = aws_transfer_server.server.arn
+}
+
+output "server_endpoint" {
+  description = "The endpoint of the transfer family server"
+  value       = aws_transfer_server.server.endpoint
+}
+
+output "server_host_key_fingerprint" {
+  description = "The RSA private key fingerprint of the transfer family server"
+  value       = aws_transfer_server.server.host_key_fingerprint
+}
+
+output "server_id" {
+  description = "The ID of the transfer family server"
+  value       = aws_transfer_server.server.id
+}
