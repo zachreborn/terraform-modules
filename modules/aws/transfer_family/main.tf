6f5bbfccfc44e63a242e62d6e82cddf3e5fa90cc
Zachary Hill <zhill@zacharyhill.co>
2024-03-21 08:04:30 -0600

    Feature - Transfer family module (#47)
    * updates to module_template
    
    * added files for transfer_family module
    
    * initial module build
    
    * removing workflow for initial build
    
    * cleanup of workflow
    
    * Merge branch 'main' into transfer_family_module
    
    * correcting protocols validation
    
    * added example
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * cleanup
    
    * Merge branch 'main' into transfer_family_module
    
    * added s3 bucket, iam role, and transfer family user
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * corrected the module name in the main.tf file
    
    * corrections for outputs
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * Merge branch 'main' into transfer_family_module
    
    * added common variables comment to template
    
    * correcting the hom directory location
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * updated the object variables for var.users
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * updated from name to arn
    
    * testing PATH instead of LOGICAL
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * added logic for mappings and logical structure
    
    * updated iam policy for more permissions to transfer family module
    
    * terraform fmt --recursive
    
    * updated comments
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * period
    
    * embarrassing typo of optional
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * recreating with the current target, switched to each variable
    
    * added ssh keys
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * added module name
    
    * added outputs for the transfer_family_server
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * added better readme example
    
    * corrected outputs to point to the correct resource

---
+++ modules/aws/transfer_family/main.tf
@@ -0,0 +1,185 @@
+###########################
+# Provider Configuration
+###########################
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+###########################
+# Data Sources
+###########################
+
+
+###########################
+# Locals
+###########################
+
+###########################
+# Module Configuration
+###########################
+
+##############
+# Create logging IAM role
+##############
+
+##############
+# Create CloudWatch log group
+##############
+
+##############
+# Create the AWS transfer family server
+##############
+
+resource "aws_transfer_server" "this" {
+  certificate                      = var.certificate
+  directory_id                     = var.directory_id
+  domain                           = var.storage_location
+  endpoint_type                    = var.endpoint_type
+  function                         = var.function
+  host_key                         = var.host_key
+  identity_provider_type           = var.identity_provider_type
+  invocation_role                  = var.invocation_role
+  logging_role                     = var.logging_role
+  pre_authentication_login_banner  = var.pre_authentication_login_banner
+  post_authentication_login_banner = var.post_authentication_login_banner
+  protocols                        = var.protocols
+  security_policy_name             = var.security_policy_name
+  url                              = var.url
+  tags                             = merge(var.tags, { "Name" = var.name })
+
+  dynamic "endpoint_details" {
+    for_each = var.endpoint_type == "VPC" ? [1] : []
+    content {
+      address_allocation_ids = var.address_allocation_ids
+      security_group_ids     = var.security_group_ids
+      subnet_ids             = var.subnet_ids
+      vpc_endpoint_id        = var.vpc_endpoint_id
+      vpc_id                 = var.vpc_id
+    }
+  }
+  protocol_details {
+    as2_transports              = var.as2_transports
+    passive_ip                  = var.passive_ip
+    set_stat_option             = var.set_stat_option
+    tls_session_resumption_mode = var.tls_session_resumption_mode
+  }
+}
+
+##############
+# Create the S3 bucket(s) for the transfer family server
+##############
+
+module "bucket" {
+  source = "../s3/bucket"
+
+  bucket_prefix = var.name
+  tags          = var.tags
+}
+
+##############
+# Create the transfer family server IAM role
+##############
+
+module "transfer_family_iam_role_policy" {
+  source = "../iam/policy"
+
+  description = "Transfer Family Server IAM role policy for ${var.name}"
+  name_prefix = "${var.name}-transfer-family-role-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid = "AllowListingOfUserFolder",
+        Action = [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ],
+        Effect   = "Allow",
+        Resource = [module.bucket.s3_bucket_arn]
+      },
+      {
+        Sid = "HomeDirObjectAccess",
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectACL",
+          "s3:GetObjectVersion",
+          "s3:DeleteObject",
+          "s3:DeleteObjectVersion",
+          "s3:PutObject",
+          "s3:PutObjectACL"
+        ],
+        Effect   = "Allow",
+        Resource = "${module.bucket.s3_bucket_arn}/*"
+      }
+    ]
+  })
+  tags = var.tags
+}
+
+module "transfer_family_iam_role" {
+  source = "../iam/role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "transfer.amazonaws.com"
+        }
+      }
+    ]
+  })
+  description = "Transfer Family Server IAM role for ${var.name}"
+  name_prefix = "${var.name}-transfer-family-role"
+  policy_arns = [module.transfer_family_iam_role_policy.arn]
+  tags        = var.tags
+}
+
+##############
+# Create the transfer family server users
+##############
+
+resource "aws_transfer_user" "this" {
+  for_each = var.users
+
+  home_directory      = each.value.home_directory
+  home_directory_type = each.value.home_directory_type
+  policy              = each.value.policy
+  role                = module.transfer_family_iam_role.arn
+  server_id           = aws_transfer_server.this.id
+  tags                = var.tags
+  user_name           = each.value.username
+
+  dynamic "home_directory_mappings" {
+    # Disables the dynamic block of home_directory_mappings if home_directory_type is not "LOGICAL".
+    for_each = each.value.home_directory_type == "LOGICAL" ? [1] : []
+    content {
+      entry  = "/"
+      target = "/${module.bucket.s3_bucket_id}/${each.value.username}"
+    }
+  }
+}
+
+##############
+# Create the user SSH keys
+##############
+
+resource "aws_transfer_ssh_key" "this" {
+  for_each = var.users
+
+  server_id = aws_transfer_server.this.id
+  user_name = each.value.username
+  body      = each.value.public_key
+}
+
+##############
+# Create the transfer family server workflow
+##############
---
+++ modules/aws/transfer_family/outputs.tf
@@ -0,0 +1,23 @@
+###########################
+# Resource Outputs
+###########################
+
+output "server_arn" {
+  description = "The ARN of the transfer family server"
+  value       = aws_transfer_server.this.arn
+}
+
+output "server_endpoint" {
+  description = "The endpoint of the transfer family server"
+  value       = aws_transfer_server.this.endpoint
+}
+
+output "server_host_key_fingerprint" {
+  description = "The RSA private key fingerprint of the transfer family server"
+  value       = aws_transfer_server.this.host_key_fingerprint
+}
+
+output "server_id" {
+  description = "The ID of the transfer family server"
+  value       = aws_transfer_server.this.id
+}
