b863a53b8d250033654be6f2d0a84e770ad28bfb
Zachary Hill <zhill@octo.ventures>
2024-03-28 19:09:54 -0400

    updated each lifecycle_rule argument to have a default value of []

--- modules/aws/transfer_family/variables.tf
+++ modules/aws/transfer_family/variables.tf
@@ -183,27 +183,27 @@ variable "lifecycle_rules" {
     status = string
     abort_incomplete_multipart_upload = optional(object({
       days_after_initiation = number
-    }))
+    }), [])
     expiration = optional(object({
       date                         = optional(string)
       days                         = optional(number)
       expired_object_delete_marker = optional(bool)
-    }))
+    }), [])
     filter = optional(object({
       object_size_greater_than = optional(number)
       object_size_less_than    = optional(number)
       prefix                   = optional(string)
       tag                      = optional(map(string))
-    }))
+    }), [])
     noncurrent_version_expiration = optional(object({
       newer_noncurrent_versions = optional(number)
       noncurrent_days           = optional(number)
-    }))
+    }), [])
     noncurrent_version_transitions = optional(list(object({
       newer_noncurrent_versions = optional(number)
       noncurrent_days           = optional(number)
       storage_class             = optional(string)
-    })))
+    })), [])
     transition = optional(list(object({
       date          = optional(string)
       days          = optional(number)
