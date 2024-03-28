22135770546723250f90e194efbaf79922330e4f
Zachary Hill <zhill@zacharyhill.co>
2024-03-28 16:58:19 -0600

    added lifecycle variable type and structure (#55)
    * added lifecycle variable type and structure
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * Made all lifecycle_rules optional
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * added transition as optional list
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * correcting optional null value for transition
    
    * Continuous Integration - terraform fmt and terraform-docs
    
    * updated readme examples
    
    ---------
    
    Co-authored-by: zachreborn <zachreborn@users.noreply.github.com>

--- modules/aws/transfer_family/variables.tf
+++ modules/aws/transfer_family/variables.tf
@@ -176,6 +176,42 @@ variable "vpc_id" {
 # S3 Bucket Variables
 ###########################
 
+variable "lifecycle_rules" {
+  description = "(Optional) Configuration of object lifecycle management (LCM). Can have several rules as a list of maps where each map is the lifecycle rule configuration."
+  type = list(object({
+    id     = string
+    status = string
+    abort_incomplete_multipart_upload = optional(object({
+      days_after_initiation = number
+    }))
+    expiration = optional(object({
+      date                         = optional(string)
+      days                         = optional(number)
+      expired_object_delete_marker = optional(bool)
+    }))
+    filter = optional(object({
+      object_size_greater_than = optional(number)
+      object_size_less_than    = optional(number)
+      prefix                   = optional(string)
+      tag                      = optional(map(string))
+    }))
+    noncurrent_version_expiration = optional(object({
+      newer_noncurrent_versions = optional(number)
+      noncurrent_days           = optional(number)
+    }))
+    noncurrent_version_transitions = optional(list(object({
+      newer_noncurrent_versions = optional(number)
+      noncurrent_days           = optional(number)
+      storage_class             = optional(string)
+    })))
+    transition = optional(list(object({
+      date          = optional(string)
+      days          = optional(number)
+      storage_class = optional(string)
+    })), [])
+  }))
+  default = null
+}
 
 ###########################
 # User Variables
@@ -192,7 +228,6 @@ variable "users" {
   }))
   default = {}
 }
-
 
 ###########################
 # General Variables
