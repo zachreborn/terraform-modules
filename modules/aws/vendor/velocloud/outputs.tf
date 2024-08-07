560c54d50e172a1451d09827e174819a314f3dd1
Zachary Hill <zhill@octo.ventures>
2024-08-07 16:26:10 -0400

    updated outputs with correct order and new mgmt

--- modules/aws/vendor/velocloud/outputs.tf
+++ modules/aws/vendor/velocloud/outputs.tf
@@ -1,13 +1,25 @@
-output "eip_id" {
+output "ec2_instance_id" {
+  value = aws_instance.ec2_instance[*].id
+}
+
+output "mgmt_eip_id" {
+  value = aws_eip.mgmt_external_ip[*].id
+}
+
+output "mgmt_eip_ip" {
+  value = aws_eip.mgmt_external_ip[*].public_ip
+}
+
+output "public_eip_id" {
   value = aws_eip.wan_external_ip[*].id
 }
 
-output "eip_public_ip" {
+output "public_eip_ip" {
   value = aws_eip.wan_external_ip[*].public_ip
 }
 
-output "ec2_instance_id" {
-  value = aws_instance.ec2_instance[*].id
+output "mgmt_network_interface_id" {
+  value = aws_network_interface.mgmt_nic[*].id
 }
 
 output "public_network_interface_id" {
@@ -17,7 +29,3 @@ output "public_network_interface_id" {
 # output "private_network_interface_id" {
 #   value = aws_network_interface.private_nic[*].id
 # }
-
-output "mgmt_network_interface_id" {
-  value = aws_network_interface.mgmt_nic[*].id
-}
