8f3d15754b8de000616ee2c03099a9244f2daac1
Zachary Hill <zhill@octo.ventures>
2024-08-16 11:20:36 -0400

    fixed user_data argument

--- modules/aws/vendor/velocloud/main.tf
+++ modules/aws/vendor/velocloud/main.tf
@@ -185,7 +185,7 @@ resource "aws_instance" "ec2_instance" {
   monitoring           = var.monitoring
   volume_tags          = merge(var.tags, ({ "Name" = format("%s%d", var.instance_name_prefix, count.index + 1) }))
   tags                 = merge(var.tags, ({ "Name" = format("%s%d", var.instance_name_prefix, count.index + 1) }))
-  user_data_base64 = templatefile("${path.module}/user_data.sh", {
+  user_data = templatefile("${path.module}/user_data.sh", {
     velocloud_activation_key     = var.velocloud_activation_key
     velocloud_ignore_cert_errors = var.velocloud_ignore_cert_errors
     velocloud_orchestrator       = var.velocloud_orchestrator
