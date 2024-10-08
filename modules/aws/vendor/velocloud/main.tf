fb5cb434a84a4e92bc49c5b5f26ea564bb91db1f
Zachary Hill <zhill@octo.ventures>
2024-10-08 11:51:05 -0400

    reverting back to the original cloudformation template

--- modules/aws/vendor/velocloud/main.tf
+++ modules/aws/vendor/velocloud/main.tf
@@ -103,16 +103,16 @@ resource "aws_security_group" "velocloud_lan_sg" {
 # EIP
 ############################################
 
-# resource "aws_eip" "mgmt_external_ip" {
-#   count  = var.number
-#   domain = "vpc"
-# }
+resource "aws_eip" "mgmt_external_ip" {
+  count  = var.number
+  domain = "vpc"
+}
 
-# resource "aws_eip_association" "mgmt_external_ip" {
-#   count                = var.number
-#   allocation_id        = element(aws_eip.mgmt_external_ip[*].id, count.index)
-#   network_interface_id = element(aws_network_interface.mgmt_nic[*].id, count.index)
-# }
+resource "aws_eip_association" "mgmt_external_ip" {
+  count                = var.number
+  allocation_id        = element(aws_eip.mgmt_external_ip[*].id, count.index)
+  network_interface_id = element(aws_network_interface.mgmt_nic[*].id, count.index)
+}
 
 resource "aws_eip" "wan_external_ip" {
   count  = var.number
@@ -131,23 +131,23 @@ resource "aws_eip_association" "wan_external_ip" {
 ############################################
 
 # Removed during testing period to match the Mettel Velocloud template
-# resource "aws_network_interface" "mgmt_nic" {
-#   # Ge1 is the management interface in VeloCloud and attached at eth0
-#   count             = var.number
-#   description       = var.mgmt_nic_description
-#   private_ips       = var.mgmt_ips == null ? null : [element(var.mgmt_ips, count.index)]
-#   security_groups   = [aws_security_group.sdwan_mgmt_sg.id]
-#   source_dest_check = var.source_dest_check
-#   subnet_id         = element(var.public_subnet_ids, count.index)
-#   tags              = merge(var.tags, ({ "Name" = format("%s%d_mgmt", var.instance_name_prefix, count.index + 1) }))
-#   attachment {
-#     instance     = element(aws_instance.ec2_instance[*].id, count.index)
-#     device_index = 0
-#   }
-# }
+resource "aws_network_interface" "mgmt_nic" {
+  # Ge1 is the management interface in VeloCloud and attached at eth0
+  count             = var.number
+  description       = var.mgmt_nic_description
+  private_ips       = var.mgmt_ips == null ? null : [element(var.mgmt_ips, count.index)]
+  security_groups   = [aws_security_group.sdwan_mgmt_sg.id]
+  source_dest_check = var.source_dest_check
+  subnet_id         = element(var.public_subnet_ids, count.index)
+  tags              = merge(var.tags, ({ "Name" = format("%s%d_mgmt", var.instance_name_prefix, count.index + 1) }))
+  attachment {
+    instance     = element(aws_instance.ec2_instance[*].id, count.index)
+    device_index = 0
+  }
+}
 
 resource "aws_network_interface" "public_nic" {
-  # Ge1 is the public interface in VeloCloud and attached at eth0
+  # Ge2 is the public interface in VeloCloud and attached at eth1
   count             = var.number
   description       = var.public_nic_description
   private_ips       = var.public_ips == null ? null : [element(var.public_ips, count.index)]
@@ -155,10 +155,14 @@ resource "aws_network_interface" "public_nic" {
   source_dest_check = var.source_dest_check
   subnet_id         = element(var.public_subnet_ids, count.index)
   tags              = merge(var.tags, ({ "Name" = format("%s%d_public", var.instance_name_prefix, count.index + 1) }))
+  attachment {
+    instance     = element(aws_instance.ec2_instance[*].id, count.index)
+    device_index = 1
+  }
 }
 
 resource "aws_network_interface" "private_nic" {
-  # Ge2 is the private interface in VeloCloud and attached at eth1
+  # Ge3 is the private interface in VeloCloud and attached at eth2
   count             = var.number
   description       = var.private_nic_description
   private_ips       = var.private_ips == null ? null : [element(var.private_ips, count.index)]
@@ -169,7 +173,7 @@ resource "aws_network_interface" "private_nic" {
 
   attachment {
     instance     = element(aws_instance.ec2_instance[*].id, count.index)
-    device_index = 1
+    device_index = 2
   }
 }
 
@@ -200,7 +204,7 @@ resource "aws_instance" "ec2_instance" {
   }
 
   network_interface {
-    network_interface_id = element(aws_network_interface.public_nic[*].id, count.index)
+    network_interface_id = element(aws_network_interface.mgmt_nic[*].id, count.index)
     device_index         = 0
   }
 
--- modules/aws/vendor/velocloud/outputs.tf
+++ modules/aws/vendor/velocloud/outputs.tf
@@ -2,13 +2,13 @@ output "ec2_instance_id" {
   value = aws_instance.ec2_instance[*].id
 }
 
-# output "mgmt_eip_id" {
-#   value = aws_eip.mgmt_external_ip[*].id
-# }
+output "mgmt_eip_id" {
+  value = aws_eip.mgmt_external_ip[*].id
+}
 
-# output "mgmt_eip_ip" {
-#   value = aws_eip.mgmt_external_ip[*].public_ip
-# }
+output "mgmt_eip_ip" {
+  value = aws_eip.mgmt_external_ip[*].public_ip
+}
 
 output "public_eip_id" {
   value = aws_eip.wan_external_ip[*].id
@@ -18,9 +18,9 @@ output "public_eip_ip" {
   value = aws_eip.wan_external_ip[*].public_ip
 }
 
-# output "mgmt_network_interface_id" {
-#   value = aws_network_interface.mgmt_nic[*].id
-# }
+output "mgmt_network_interface_id" {
+  value = aws_network_interface.mgmt_nic[*].id
+}
 
 output "public_network_interface_id" {
   value = aws_network_interface.public_nic[*].id
