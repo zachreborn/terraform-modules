858e42003239151006ca7b2b217bbf0bf6203381
Zachary Hill <zhill@octo.ventures>
2024-11-01 09:50:32 -0400

    cleaning up user_data and added base64encode

--- modules/aws/vendor/velocloud/main.tf
+++ modules/aws/vendor/velocloud/main.tf
@@ -167,11 +167,11 @@ resource "aws_instance" "ec2_instance" {
   monitoring           = var.monitoring
   volume_tags          = merge(var.tags, ({ "Name" = format("%s%d", var.instance_name_prefix, count.index + 1) }))
   tags                 = merge(var.tags, ({ "Name" = format("%s%d", var.instance_name_prefix, count.index + 1) }))
-  user_data = var.user_data != null ? var.user_data : templatefile("${path.module}/user_data.tftpl", {
+  user_data = var.user_data != null ? var.user_data : base64encode(templatefile("${path.module}/user_data.tftpl", {
     velocloud_activation_key     = var.velocloud_activation_key
     velocloud_ignore_cert_errors = var.velocloud_ignore_cert_errors
     velocloud_orchestrator       = var.velocloud_orchestrator
-  })
+  }))
 
   metadata_options {
     http_endpoint = var.http_endpoint
