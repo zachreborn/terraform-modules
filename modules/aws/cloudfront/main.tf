6e90f176bdedd1c7d3a5de38729673b6bc4f1d6a
Zachary Hill <zhill@octo.ventures>
2024-11-16 15:41:31 -0500

    Revert "converted to key lookups"
    This reverts commit 1771a159fdcf4b4de472cdff4cb0c8a6dee7deb7.

--- modules/aws/cloudfront/main.tf
+++ modules/aws/cloudfront/main.tf
@@ -45,10 +45,10 @@ resource "aws_cloudfront_distribution" "this" {
   dynamic "custom_error_response" {
     for_each = var.custom_error_response != null ? var.custom_error_response : {}
     content {
-      error_caching_min_ttl = custom_error_response.value["error_caching_min_ttl"]
-      error_code            = custom_error_response.value["error_code"]
-      response_code         = custom_error_response.value["response_code"]
-      response_page_path    = custom_error_response.value["response_page_path"]
+      error_caching_min_ttl = custom_error_response.value.error_caching_min_ttl
+      error_code            = custom_error_response.value.error_code
+      response_code         = custom_error_response.value.response_code
+      response_page_path    = custom_error_response.value.response_page_path
     }
   }
 
@@ -82,22 +82,22 @@ resource "aws_cloudfront_distribution" "this" {
   dynamic "ordered_cache_behavior" {
     for_each = var.ordered_cache_behavior != null ? var.ordered_cache_behavior : {}
     content {
-      allowed_methods           = ordered_cache_behavior.value["allowed_methods"]
-      cached_methods            = ordered_cache_behavior.value["cached_methods"]
-      cache_policy_id           = ordered_cache_behavior.value["cache_policy_id"]
-      compress                  = ordered_cache_behavior.value["compress"]
-      field_level_encryption_id = ordered_cache_behavior.value["field_level_encryption_id"]
+      allowed_methods           = ordered_cache_behavior.value.allowed_methods
+      cached_methods            = ordered_cache_behavior.value.cached_methods
+      cache_policy_id           = ordered_cache_behavior.value.cache_policy_id
+      compress                  = ordered_cache_behavior.value.compress
+      field_level_encryption_id = ordered_cache_behavior.value.field_level_encryption_id
       # lambda_function_association block
       # function_association block
-      origin_request_policy_id   = ordered_cache_behavior.value["origin_request_policy_id"]
-      path_pattern               = ordered_cache_behavior.value["path_pattern"]
-      realtime_log_config_arn    = ordered_cache_behavior.value["realtime_log_config_arn"]
-      response_headers_policy_id = ordered_cache_behavior.value["response_headers_policy_id"]
-      smooth_streaming           = ordered_cache_behavior.value["smooth_streaming"]
-      target_origin_id           = ordered_cache_behavior.value["target_origin_id"]
-      trusted_key_groups         = ordered_cache_behavior.value["trusted_key_groups"]
-      trusted_signers            = ordered_cache_behavior.value["trusted_signers"]
-      viewer_protocol_policy     = ordered_cache_behavior.value["viewer_protocol_policy"]
+      origin_request_policy_id   = ordered_cache_behavior.value.origin_request_policy_id
+      path_pattern               = ordered_cache_behavior.value.path_pattern
+      realtime_log_config_arn    = ordered_cache_behavior.value.realtime_log_config_arn
+      response_headers_policy_id = ordered_cache_behavior.value.response_headers_policy_id
+      smooth_streaming           = ordered_cache_behavior.value.smooth_streaming
+      target_origin_id           = ordered_cache_behavior.value.target_origin_id
+      trusted_key_groups         = ordered_cache_behavior.value.trusted_key_groups
+      trusted_signers            = ordered_cache_behavior.value.trusted_signers
+      viewer_protocol_policy     = ordered_cache_behavior.value.viewer_protocol_policy
     }
   }
 
@@ -114,35 +114,35 @@ resource "aws_cloudfront_distribution" "this" {
       dynamic "custom_header" {
         for_each = origin.value.custom_header != null ? origin.value.custom_header : {}
         content {
-          name  = custom_header.value["header_name"]
-          value = custom_header.value["header_value"]
+          name  = custom_header.header_name
+          value = custom_header.header_value
         }
       }
 
       dynamic "custom_origin_config" {
         for_each = origin.value.custom_origin_config != null ? origin.value.custom_origin_config : null
         content {
-          http_port                = custom_origin_config.value["http_port"]
-          https_port               = custom_origin_config.value["https_port"]
-          origin_keepalive_timeout = custom_origin_config.value["origin_keepalive_timeout"]
-          origin_protocol_policy   = custom_origin_config.value["origin_protocol_policy"]
-          origin_read_timeout      = custom_origin_config.value["origin_read_timeout"]
-          origin_ssl_protocols     = custom_origin_config.value["origin_ssl_protocols"]
+          http_port                = custom_origin_config.value.http_port
+          https_port               = custom_origin_config.value.https_port
+          origin_keepalive_timeout = custom_origin_config.value.origin_keepalive_timeout
+          origin_protocol_policy   = custom_origin_config.value.origin_protocol_policy
+          origin_read_timeout      = custom_origin_config.value.origin_read_timeout
+          origin_ssl_protocols     = custom_origin_config.value.origin_ssl_protocols
         }
       }
 
       dynamic "origin_shield" {
         for_each = origin.value.origin_shield != null ? origin.value.origin_shield : {}
         content {
-          enabled              = origin_shield.value["enabled"]
-          origin_shield_region = origin_shield.value["origin_shield_region"]
+          enabled              = origin_shield.enabled
+          origin_shield_region = origin_shield.origin_shield_region
         }
       }
 
       dynamic "s3_origin_config" {
         for_each = origin.value.s3_origin_config != null ? origin.value.s3_origin_config : {}
         content {
-          origin_access_identity = s3_origin_config.value["origin_access_identity"]
+          origin_access_identity = s3_origin_config.origin_access_identity
         }
       }
     }
