1a44d3c84111b9e100b1685a6352ebb56b21b41b
Zachary Hill <zhill@octo.ventures>
2024-11-29 17:26:44 -0500

    corrected vpc_id syntax

--- modules/aws/transit_gateway/attachment/main.tf
+++ modules/aws/transit_gateway/attachment/main.tf
@@ -21,7 +21,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   transit_gateway_id                              = var.transit_gateway_id
   transit_gateway_default_route_table_association = var.transit_gateway_default_route_table_association
   transit_gateway_default_route_table_propagation = var.transit_gateway_default_route_table_propagation
-  vpc_id                                          = each.value
+  vpc_id                                          = each.value.vpc_id
 }
 
 ###########################
