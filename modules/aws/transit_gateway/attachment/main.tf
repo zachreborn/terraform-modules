be3872d14e3ff6c42c8df1b2d507981c340644cb
Zachary Hill <zhill@octo.ventures>
2024-11-29 16:58:40 -0500

    feat: Refactor tgw attachment module BREAKING CHANGE - Added `for_each` to the `../transit_gateway/attachment` module - Removed `../direct_connect_connection` module - Updated `../transit_gateway/attachment` module to use `for_each` instead of `count` - Updated the `vpc_id` variable to `vpc_ids` in the `../transit_gateway/attachment` module - `vpc_ids` is a map of objects that contain the `vpc_id` and `subnet_ids` for each VPC

--- modules/aws/transit_gateway/attachment/main.tf
+++ modules/aws/transit_gateway/attachment/main.tf
@@ -11,28 +11,26 @@ terraform {
 ###########################
 # Transit Gateway Attachment
 ###########################
-
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  appliance_mode_support                          = var.appliance_mode_support
-  dns_support                                     = var.dns_support
-  ipv6_support                                    = var.ipv6_support
-  subnet_ids                                      = var.subnet_ids
-  tags                                            = merge(tomap({ Name = var.name }), var.tags)
+  for_each                                        = var.vpc_ids
+  appliance_mode_support                          = each.value.appliance_mode_support
+  dns_support                                     = each.value.dns_support
+  ipv6_support                                    = each.value.ipv6_support
+  subnet_ids                                      = each.value.subnet_ids
+  tags                                            = merge(tomap({ Name = each.key }), var.tags)
   transit_gateway_id                              = var.transit_gateway_id
   transit_gateway_default_route_table_association = var.transit_gateway_default_route_table_association
   transit_gateway_default_route_table_propagation = var.transit_gateway_default_route_table_propagation
-  vpc_id                                          = var.vpc_id
+  vpc_id                                          = each.value
 }
-
 
 ###########################
 # Flow Logs
 ###########################
-
 module "vpc_flow_logs" {
   source = "../../flow_logs"
 
-  count                              = var.enable_flow_logs ? 1 : 0
+  for_each                           = var.enable_flow_logs ? var.vpc_ids : {}
   cloudwatch_name_prefix             = var.cloudwatch_name_prefix
   cloudwatch_retention_in_days       = var.cloudwatch_retention_in_days
   iam_policy_name_prefix             = var.iam_policy_name_prefix
