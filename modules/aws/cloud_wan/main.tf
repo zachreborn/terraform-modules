5ec40b8ea62203415ca0306db7181d1b72b4b9c3
Zachary Hill <zhill@octo.ventures>
2024-12-01 15:35:27 -0500

    initial commit

---
+++ modules/aws/cloud_wan/main.tf
@@ -0,0 +1,37 @@
+###########################
+# Provider Configuration
+###########################
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+###########################
+# Data Sources
+###########################
+
+
+###########################
+# Locals
+###########################
+
+###########################
+# Module Configuration
+###########################
+
+resource "aws_networkmanager_global_network" "this" {
+  description = var.description
+  tags        = var.tags
+}
+
+resource "aws_networkmanager_core_network" "this" {
+  base_policy_regions = var.base_policy_regions
+  description         = var.description
+  global_network_id   = aws_networkmanager_global_network.this.id
+  tags                = var.tags
+}
