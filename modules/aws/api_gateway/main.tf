e0d1143b61cf0e6975ed9eae3a26a311aeb9fc45
jakeasaurus <jjones@slfcu.org>
2024-12-03 15:54:00 -0700

    missed some saves

--- modules/aws/api_gateway/main.tf
+++ modules/aws/api_gateway/main.tf
@@ -18,65 +18,28 @@ data "aws_region" "current" {}
 # API Gateway
 #############################
 resource "aws_apigatewayv2_api" "example" {
-  name                       = var.name 
+  #Required
+  name                       = var.name
   protocol_type              = var.protocol_type
   route_selection_expression = "$request.body.action"
-}
 
-###################################################
-CloudWatch Alarms
-###################################################
+  #Optional
+  api_key_selection_expression = var.api_key_selection_expression
+  cors_configuration {
+      allow_credentials = lookup(var.cors_configuration, "allow_credentials", null)
+      allow_headers    = lookup(var.cors_configuration, "allow_headers", null)
+      allow_methods    = lookup(var.cors_configuration, "allow_methods", null)
+      allow_origins    = lookup(var.cors_configuration, "allow_origins", null)
+      expose_headers   = lookup(var.cors_configuration, "expose_headers", null)
+      max_age         = lookup(var.cors_configuration, "max_age", null)
+    }
+  credentials_arn          = var.credentials_arn
+  description             = var.description
+  disable_execute_api_endpoint = var.disable_execute_api_endpoint
+  fail_on_warnings        = var.fail_on_warnings
+  tags                    = var.tags
+  target                  = var.target
+  version                 = var.version
+  body                   = var.body
 
-#####################
-# Status Check Failed Instance Metric
-#####################
-
-resource "aws_cloudwatch_metric_alarm" "instance" {
-  actions_enabled     = true
-  alarm_actions       = []
-  alarm_description   = "EC2 instance StatusCheckFailed_Instance alarm"
-  alarm_name          = format("%s-instance-alarm", aws_instance.ec2[count.index].id)
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  count               = var.number
-  datapoints_to_alarm = 2
-  dimensions = {
-    InstanceId = aws_instance.ec2[count.index].id
-  }
-  evaluation_periods        = "2"
-  insufficient_data_actions = []
-  metric_name               = "StatusCheckFailed_Instance"
-  namespace                 = "AWS/EC2"
-  ok_actions                = []
-  period                    = "60"
-  statistic                 = "Maximum"
-  threshold                 = "1"
-  treat_missing_data        = "missing"
-  #unit                      = var.unit
-}
-
-#####################
-# Status Check Failed System Metric
-#####################
-
-resource "aws_cloudwatch_metric_alarm" "system" {
-  actions_enabled     = true
-  alarm_actions       = ["arn:aws:automate:${data.aws_region.current.name}:ec2:recover"]
-  alarm_description   = "EC2 instance StatusCheckFailed_System alarm"
-  alarm_name          = format("%s-system-alarm", aws_instance.ec2[count.index].id)
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  count               = var.number
-  datapoints_to_alarm = 2
-  dimensions = {
-    InstanceId = aws_instance.ec2[count.index].id
-  }
-  evaluation_periods        = "2"
-  insufficient_data_actions = []
-  metric_name               = "StatusCheckFailed_System"
-  namespace                 = "AWS/EC2"
-  ok_actions                = []
-  period                    = "60"
-  statistic                 = "Maximum"
-  threshold                 = "1"
-  treat_missing_data        = "missing"
-  #unit                      = var.unit
 }
--- modules/aws/api_gateway/variables.tf
+++ modules/aws/api_gateway/variables.tf
@@ -1,1 +1,91 @@
+############################################
+# Required
+############################################
 
+variable "name" {
+  description = "Name of the API Gateway"
+  type        = string
+}
+
+variable "protocol_type" {
+  description = "Protocol type of the API Gateway (HTTP or WEBSOCKET)"
+  type        = string
+}
+
+############################################
+# Optional
+############################################
+
+variable "api_key_selection_expression" {
+  description = "API key selection expression for the API Gateway"
+  type        = string
+  default     = ""
+}
+
+variable "cors_configuration" {
+  description = "CORS configuration for the API Gateway"
+  type        = object({
+    allow_credentials = bool
+    allow_headers    = list(string)
+    allow_methods    = list(string)
+    allow_origins    = list(string)
+    expose_headers   = list(string)
+    max_age          = number
+  })
+  default     = {
+    allow_credentials = false
+    allow_headers    = []
+    allow_methods    = []
+    allow_origins    = []
+    expose_headers   = []
+    max_age          = 0
+  }
+}
+
+variable "credentials_arn" {
+  description = "ARN of the credentials for the API Gateway"
+  type        = string
+  default     = null
+}
+
+variable "description" {
+  description = "Description of the API Gateway"
+  type        = string
+  default     = null
+}
+
+variable "disable_execute_api_endpoint" {
+  description = "Whether to disable the execute-api endpoint"
+  type        = bool
+  default     = false
+}
+
+variable "fail_on_warnings" {
+  description = "Whether to fail on warnings during API Gateway creation"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Tags to apply to the API Gateway"
+  type        = map(string)
+  default     = {}
+}
+
+variable "target" {
+  description = "Target for the API Gateway"
+  type        = string
+  default     = null
+}
+
+variable "version" {
+  description = "Version of the API Gateway"
+  type        = string
+  default     = null
+}
+
+variable "body" {
+  description = "OpenAPI specification for the API Gateway"
+  type        = string
+  default     = null
+}
