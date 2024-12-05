ebeac005fd643fedbc823c29bff5f14ff2398c24
Zachary Hill <zhill@octo.ventures>
2024-12-05 09:35:20 -0700

    added cloudwan initial config

--- modules/aws/cloud_wan/main.tf
+++ modules/aws/cloud_wan/main.tf
@@ -29,9 +29,19 @@ resource "aws_networkmanager_global_network" "this" {
   tags        = var.tags
 }
 
-resource "aws_networkmanager_core_network" "this" {
-  base_policy_regions = var.base_policy_regions
-  description         = var.description
+# resource "aws_networkmanager_core_network" "this" {
+#   base_policy_regions = var.base_policy_regions
+#   description         = var.description
+#   global_network_id   = aws_networkmanager_global_network.this.id
+#   tags                = var.tags
+# }
+
+###########################
+# Transit Gateway
+###########################
+
+resource "aws_networkmanager_transit_gateway_registration" "this" {
+  for_each            = toset(var.transit_gateway_arns)
   global_network_id   = aws_networkmanager_global_network.this.id
-  tags                = var.tags
+  transit_gateway_arn = each.key
 }
--- modules/aws/cloud_wan/variables.tf
+++ modules/aws/cloud_wan/variables.tf
@@ -2,7 +2,30 @@
 # Resource Variables
 ###########################
 
+variable "description" {
+  description = "(Required) The description of the global network"
+  type        = string
+}
+
+###########################
+# Transit Gateway Variables
+###########################
+
+variable "transit_gateway_arns" {
+  description = "(Required) List of ARNs of the transit gateways to register with the global network"
+  type        = list(string)
+}
+
 ###########################
 # General Variables
 ###########################
 
+variable "tags" {
+  description = "(Optional) Map of tags to assign to the device."
+  type        = map(any)
+  default = {
+    created_by  = "terraform" # Your name goes here
+    terraform   = "true"
+    environment = "prod"
+  }
+}
