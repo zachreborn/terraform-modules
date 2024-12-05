3880207ce0e7ea7b8f95bf867f9c709cb3868008
jakeasaurus <jjones@slfcu.org>
2024-12-05 13:36:47 -0700

    adding back in after reset

--- modules/aws/api_gateway/outputs.tf
+++ modules/aws/api_gateway/outputs.tf
@@ -4,35 +4,35 @@
 
 output "id" {
   description = "The API identifier"
-  value       = aws_apigatewayv2_api.example.id
+  value       = aws_apigatewayv2_api.api.id
 }
 
 output "api_endpoint" {
   description = "The URI of the API"
-  value       = aws_apigatewayv2_api.example.api_endpoint
+  value       = aws_apigatewayv2_api.api.api_endpoint
 }
 
 output "arn" {
   description = "The ARN of the API"
-  value       = aws_apigatewayv2_api.example.arn
+  value       = aws_apigatewayv2_api.api.arn
 }
 
 output "execution_arn" {
   description = "The ARN prefix to be used in permission policies"
-  value       = aws_apigatewayv2_api.example.execution_arn
+  value       = aws_apigatewayv2_api.api.execution_arn
 }
 
 output "api_key_selection_expression" {
   description = "The API key selection expression for the API"
-  value       = aws_apigatewayv2_api.example.api_key_selection_expression
+  value       = aws_apigatewayv2_api.api.api_key_selection_expression
 }
 
 output "cors_configuration" {
   description = "The CORS configuration for the API"
-  value       = aws_apigatewayv2_api.example.cors_configuration
+  value       = aws_apigatewayv2_api.api.cors_configuration
 }
 
 output "tags_all" {
   description = "Map of tags assigned to the resource"
-  value       = aws_apigatewayv2_api.example.tags_all
+  value       = aws_apigatewayv2_api.api.tags_all
 }
