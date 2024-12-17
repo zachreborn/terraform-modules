9a46a556a630dd222e90c4c9cba7ea50de3c2692
Zachary Hill <zhill@octo.ventures>
2024-12-17 07:25:06 -0500

    added resource share

--- modules/aws/ram/main.tf
+++ modules/aws/ram/main.tf
@@ -15,6 +15,8 @@ terraform {
 # Data Sources
 ###########################
 
+data "aws_organizations_organization" "current_org" {}
+
 
 ###########################
 # Locals
@@ -26,4 +28,21 @@ terraform {
 
 resource "aws_ram_sharing_with_organization" "this" {
   count = var.enable_organization_sharing ? 1 : 0
+}
+
+resource "aws_ram_resource_share" "this" {
+  allow_external_principals = var.allow_external_principals
+  name                      = var.name
+  permission_arns           = var.permission_arns
+  tags                      = var.tags
+}
+
+resource "aws_ram_resource_association" "this" {
+  resource_arn       = var.resource_arn
+  resource_share_arn = aws_ram_resource_share.this.arn
+}
+
+resource "aws_ram_principal_association" "this" {
+  principal          = var.principal != null ? var.principal : data.aws_organizations_organization.current_org.arn
+  resource_share_arn = aws_ram_resource_share.this.arn
 }
--- modules/aws/ram/variables.tf
+++ modules/aws/ram/variables.tf
@@ -2,12 +2,50 @@
 # Resource Variables
 ###########################
 
+variable "allow_external_principals" {
+  description = "Indicates whether principals outside your AWS organization can be associated with a resource share."
+  type        = bool
+  default     = false
+}
+
+variable "enable_organization_sharing" {
+  description = "Enable sharing with AWS Organizations."
+  type        = bool
+  default     = false
+}
+
+variable "name" {
+  description = "The name of the resource share."
+  type        = string
+}
+
+variable "permission_arns" {
+  description = "The ARNs of the permissions to associate with the resource share."
+  type        = list(string)
+  default     = null
+}
+
+variable "principal" {
+  description = "The principal to associate with the resource share."
+  type        = string
+  default     = null
+}
+
+variable "resource_arn" {
+  description = "The ARN of the resource to associate with the resource share."
+  type        = string
+}
+
 ###########################
 # General Variables
 ###########################
 
-variable "enable_organization_sharing" {
-  description = "Enable sharing with AWS Organizations"
-  type        = bool
-  default     = true
+variable "tags" {
+  description = "A mapping of tags to assign to the resource share."
+  type        = map(string)
+  default = {
+    created_by  = "terraform" # Your name goes here
+    terraform   = "true"
+    environment = "prod"
+  }
 }
