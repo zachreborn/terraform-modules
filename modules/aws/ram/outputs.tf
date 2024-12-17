30ec16537a4f998b28ec11ee4c6864b77930466e
Zachary Hill <zhill@octo.ventures>
2024-12-17 07:43:09 -0500

    added ram outputs

--- modules/aws/ram/outputs.tf
+++ modules/aws/ram/outputs.tf
@@ -2,3 +2,12 @@
 # Resource Outputs
 ###########################
 
+output "arn" {
+  description = "The ARN of the resource share."
+  value       = aws_ram_resource_share.this.arn
+}
+
+output "id" {
+  description = "The ID of the resource share."
+  value       = aws_ram_resource_share.this.id
+}
