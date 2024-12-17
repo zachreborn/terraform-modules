cf027884c0190eb1f3b220495c2ef257e66fd09a
Zachary Hill <zhill@octo.ventures>
2024-12-17 06:48:12 -0500

    added ram module and org setting

---
+++ modules/aws/ram/main.tf
@@ -0,0 +1,29 @@
+###########################
+# Provider Configuration
+###########################
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+###########################
+# Data Sources
+###########################
+
+
+###########################
+# Locals
+###########################
+
+###########################
+# Module Configuration
+###########################
+
+resource "aws_ram_sharing_with_organization" "this" {
+  count = var.enable_organization_sharing
+}
---
+++ modules/aws/ram/variables.tf
@@ -0,0 +1,13 @@
+###########################
+# Resource Variables
+###########################
+
+###########################
+# General Variables
+###########################
+
+variable "enable_organization_sharing" {
+  description = "Enable sharing with AWS Organizations"
+  type        = bool
+  default     = true
+}
