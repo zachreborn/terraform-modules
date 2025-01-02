1b10575a0194024e13c1cd2387d42c030c69994a
jakeasaurus <jjones@slfcu.org>
2025-01-02 12:04:15 -0500

    fixed syntax to remove all instances of lookup

--- modules/aws/lb/variables.tf
+++ modules/aws/lb/variables.tf
@@ -1,3 +1,4 @@
+# Load Balancer Variables
 variable "name" {
   description = "Name of the load balancer"
   type        = string
@@ -78,26 +79,185 @@ variable "access_logs" {
   default = null
 }
 
+# Target Group Variables
+variable "target_group_name" {
+  description = "Name of the target group"
+  type        = string
+  default     = null
+}
+
+variable "target_group_name_prefix" {
+  description = "Prefix for the target group name"
+  type        = string
+  default     = null
+}
+
+variable "target_group_port" {
+  description = "Port on which targets receive traffic"
+  type        = number
+}
+
+variable "target_group_protocol" {
+  description = "Protocol to use for routing traffic to the targets"
+  type        = string
+}
+
+variable "target_group_vpc_id" {
+  description = "Identifier of the VPC in which to create the target group"
+  type        = string
+}
+
+variable "target_group_target_type" {
+  description = "Type of target that you must specify when registering targets with this target group"
+  type        = string
+  default     = "instance"
+}
+
+variable "target_group_deregistration_delay" {
+  description = "Amount of time to wait for in-flight requests to complete before deregistering a target"
+  type        = number
+  default     = 300
+}
+
+variable "target_group_slow_start" {
+  description = "Amount of time for targets to warm up before the load balancer sends them a full share of requests"
+  type        = number
+  default     = 0
+}
+
+variable "target_group_proxy_protocol_v2" {
+  description = "Whether to enable support for proxy protocol v2"
+  type        = bool
+  default     = false
+}
+
+variable "target_group_load_balancing_algorithm_type" {
+  description = "Determines how the load balancer selects targets when routing requests"
+  type        = string
+  default     = null
+}
+
+variable "target_group_preserve_client_ip" {
+  description = "Whether client IP preservation is enabled"
+  type        = bool
+  default     = null
+}
+
+# The target_groups variable should now only contain the nested objects
+variable "target_groups" {
+  description = "Map of target group configurations"
+  type = map(object({
+    health_check = optional(object({
+      enabled             = optional(bool, true)
+      healthy_threshold   = optional(number, 3)
+      interval            = optional(number, 30)
+      matcher             = optional(string)
+      path                = optional(string)
+      port                = optional(string, "traffic-port")
+      protocol            = optional(string, "HTTP")
+      timeout             = optional(number, 5)
+      unhealthy_threshold = optional(number, 3)
+    }))
+
+    stickiness = optional(object({
+      type            = string
+      cookie_duration = optional(number)
+      cookie_name     = optional(string)
+    }))
+
+    tags = optional(map(string), {})
+  }))
+  default = {}
+}
+
+# Listener Variables
+variable "listeners" {
+  description = "Map of listener configurations"
+  type = map(object({
+    port            = number
+    protocol        = string
+    ssl_policy      = optional(string)
+    certificate_arn = optional(string)
+
+    default_action = object({
+      type             = string
+      target_group_arn = optional(string)
+
+      fixed_response = optional(object({
+        content_type = string
+        message_body = optional(string)
+        status_code  = optional(string)
+      }))
+
+      redirect = optional(object({
+        path        = optional(string)
+        host        = optional(string)
+        port        = optional(string)
+        protocol    = optional(string)
+        query       = optional(string)
+        status_code = string
+      }))
+    })
+  }))
+  default = {}
+}
+
+# Listener Rule Variables
+variable "listener_rules" {
+  description = "Map of listener rule configurations"
+  type = map(object({
+    listener_key = string
+    priority     = optional(number)
+
+    action = object({
+      type             = string
+      target_group_arn = optional(string)
+
+      fixed_response = optional(object({
+        content_type = string
+        message_body = optional(string)
+        status_code  = optional(string)
+      }))
+
+      redirect = optional(object({
+        path        = optional(string)
+        host        = optional(string)
+        port        = optional(string)
+        protocol    = optional(string)
+        query       = optional(string)
+        status_code = string
+      }))
+    })
+
+    conditions = list(object({
+      host_header = optional(object({
+        values = list(string)
+      }))
+
+      http_header = optional(object({
+        http_header_name = string
+        values           = list(string)
+      }))
+
+      path_pattern = optional(object({
+        values = list(string)
+      }))
+
+      query_string = optional(list(object({
+        key   = optional(string)
+        value = string
+      })))
+
+      source_ip = optional(object({
+        values = list(string)
+      }))
+    }))
+  }))
+  default = {}
+}
+
 variable "tags" {
   description = "A map of tags to assign to the resource"
   type        = map(string)
-  default     = {}
-}
-
-variable "target_groups" {
-  description = "Map of target group configurations"
-  type        = any
-  default     = {}
-}
-
-variable "listeners" {
-  description = "Map of listener configurations"
-  type        = any
-  default     = {}
-}
-
-variable "listener_rules" {
-  description = "Map of listener rule configurations"
-  type        = any
   default     = {}
 }
