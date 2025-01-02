316c41ff34f87f251d7327b1beb8d213661125b9
jakeasaurus <jjones@slfcu.org>
2025-01-02 12:04:15 -0500

    fixed syntax to remove all instances of lookup

--- modules/aws/lb/main.tf
+++ modules/aws/lb/main.tf
@@ -29,9 +29,9 @@ resource "aws_lb" "load_balancer" {
     for_each = var.subnet_mappings
     content {
       subnet_id            = subnet_mapping.value.subnet_id
-      allocation_id        = lookup(subnet_mapping.value, "allocation_id", null)
-      private_ipv4_address = lookup(subnet_mapping.value, "private_ipv4_address", null)
-      ipv6_address         = lookup(subnet_mapping.value, "ipv6_address", null)
+      allocation_id        = subnet_mapping.value.allocation_id
+      private_ipv4_address = subnet_mapping.value.private_ipv4_address
+      ipv6_address         = subnet_mapping.value.ipv6_address
     }
   }
 
@@ -47,45 +47,45 @@ resource "aws_lb" "load_balancer" {
 resource "aws_lb_target_group" "target_group" {
   for_each = var.target_groups
 
-  name                          = lookup(each.value, "name", null)
-  name_prefix                   = lookup(each.value, "name_prefix", null)
-  port                          = each.value.port
-  protocol                      = each.value.protocol
-  vpc_id                        = each.value.vpc_id
-  target_type                   = lookup(each.value, "target_type", "instance")
-  deregistration_delay          = lookup(each.value, "deregistration_delay", 300)
-  slow_start                    = lookup(each.value, "slow_start", 0)
-  proxy_protocol_v2             = lookup(each.value, "proxy_protocol_v2", false)
-  load_balancing_algorithm_type = lookup(each.value, "load_balancing_algorithm_type", null)
-  preserve_client_ip            = lookup(each.value, "preserve_client_ip", null)
+  name                          = var.target_group_name
+  name_prefix                   = var.target_group_name_prefix
+  port                          = var.target_group_port
+  protocol                      = var.target_group_protocol
+  vpc_id                        = var.target_group_vpc_id
+  target_type                   = var.target_group_target_type
+  deregistration_delay          = var.target_group_deregistration_delay
+  slow_start                    = var.target_group_slow_start
+  proxy_protocol_v2             = var.target_group_proxy_protocol_v2
+  load_balancing_algorithm_type = var.target_group_load_balancing_algorithm_type
+  preserve_client_ip            = var.target_group_preserve_client_ip
 
   dynamic "health_check" {
-    for_each = lookup(each.value, "health_check", null) != null ? [each.value.health_check] : []
+    for_each = each.value.health_check != null ? [each.value.health_check] : []
     content {
-      enabled             = lookup(health_check.value, "enabled", true)
-      healthy_threshold   = lookup(health_check.value, "healthy_threshold", 3)
-      interval            = lookup(health_check.value, "interval", 30)
-      matcher             = lookup(health_check.value, "matcher", null)
-      path                = lookup(health_check.value, "path", null)
-      port                = lookup(health_check.value, "port", "traffic-port")
-      protocol            = lookup(health_check.value, "protocol", "HTTP")
-      timeout             = lookup(health_check.value, "timeout", 5)
-      unhealthy_threshold = lookup(health_check.value, "unhealthy_threshold", 3)
+      enabled             = health_check.value.enabled
+      healthy_threshold   = health_check.value.healthy_threshold
+      interval            = health_check.value.interval
+      matcher             = health_check.value.matcher
+      path                = health_check.value.path
+      port                = health_check.value.port
+      protocol            = health_check.value.protocol
+      timeout             = health_check.value.timeout
+      unhealthy_threshold = health_check.value.unhealthy_threshold
     }
   }
 
   dynamic "stickiness" {
-    for_each = lookup(each.value, "stickiness", null) != null ? [each.value.stickiness] : []
+    for_each = each.value.stickiness != null ? [each.value.stickiness] : []
     content {
       type            = stickiness.value.type
-      cookie_duration = lookup(stickiness.value, "cookie_duration", null)
-      cookie_name     = lookup(stickiness.value, "cookie_name", null)
+      cookie_duration = stickiness.value.cookie_duration
+      cookie_name     = stickiness.value.cookie_name
     }
   }
 
   tags = merge(
     var.tags,
-    lookup(each.value, "tags", {}),
+    each.value.tags
   )
 }
 
@@ -96,32 +96,32 @@ resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.load_balancer.arn
   port              = each.value.port
   protocol          = each.value.protocol
-  ssl_policy        = lookup(each.value, "ssl_policy", null)
-  certificate_arn   = lookup(each.value, "certificate_arn", null)
+  ssl_policy        = each.value.ssl_policy
+  certificate_arn   = each.value
 
   dynamic "default_action" {
     for_each = [each.value.default_action]
     content {
       type             = default_action.value.type
-      target_group_arn = lookup(default_action.value, "target_group_arn", null)
+      target_group_arn = default_action.value.target_group_arn
 
       dynamic "fixed_response" {
-        for_each = lookup(default_action.value, "fixed_response", null) != null ? [default_action.value.fixed_response] : []
+        for_each = default_action.value.fixed_response != null ? [default_action.value.fixed_response] : []
         content {
           content_type = fixed_response.value.content_type
-          message_body = lookup(fixed_response.value, "message_body", null)
-          status_code  = lookup(fixed_response.value, "status_code", null)
+          message_body = fixed_response.value.message_body
+          status_code  = fixed_response.value.status_code
         }
       }
 
       dynamic "redirect" {
-        for_each = lookup(default_action.value, "redirect", null) != null ? [default_action.value.redirect] : []
+        for_each = default_action.value.redirect != null ? [default_action.value.redirect] : []
         content {
-          path        = lookup(redirect.value, "path", null)
-          host        = lookup(redirect.value, "host", null)
-          port        = lookup(redirect.value, "port", null)
-          protocol    = lookup(redirect.value, "protocol", null)
-          query       = lookup(redirect.value, "query", null)
+          path        = redirect.value.path
+          host        = redirect.value.host
+          port        = redirect.value.port
+          protocol    = redirect.value.protocol
+          query       = redirect.value.query
           status_code = redirect.value.status_code
         }
       }
@@ -134,31 +134,31 @@ resource "aws_lb_listener_rule" "listener_rule" {
   for_each = var.listener_rules
 
   listener_arn = aws_lb_listener.listener[each.value.listener_key].arn
-  priority     = lookup(each.value, "priority", null)
+  priority     = each.value.priority
 
   dynamic "action" {
     for_each = [each.value.action]
     content {
       type             = action.value.type
-      target_group_arn = lookup(action.value, "target_group_arn", null)
+      target_group_arn = action.value.target_group_arn
 
       dynamic "fixed_response" {
-        for_each = lookup(action.value, "fixed_response", null) != null ? [action.value.fixed_response] : []
+        for_each = action.value.fixed_response != null ? [action.value.fixed_response] : []
         content {
           content_type = fixed_response.value.content_type
-          message_body = lookup(fixed_response.value, "message_body", null)
-          status_code  = lookup(fixed_response.value, "status_code", null)
+          message_body = fixed_response.value.message_body
+          status_code  = fixed_response.value.status_code
         }
       }
 
       dynamic "redirect" {
-        for_each = lookup(action.value, "redirect", null) != null ? [action.value.redirect] : []
+        for_each = action.value.redirect != null ? [action.value.redirect] : []
         content {
-          path        = lookup(redirect.value, "path", null)
-          host        = lookup(redirect.value, "host", null)
-          port        = lookup(redirect.value, "port", null)
-          protocol    = lookup(redirect.value, "protocol", null)
-          query       = lookup(redirect.value, "query", null)
+          path        = redirect.value.path
+          host        = redirect.value.host
+          port        = redirect.value.port
+          protocol    = redirect.value.protocol
+          query       = redirect.value.query
           status_code = redirect.value.status_code
         }
       }
@@ -169,14 +169,14 @@ resource "aws_lb_listener_rule" "listener_rule" {
     for_each = each.value.conditions
     content {
       dynamic "host_header" {
-        for_each = lookup(condition.value, "host_header", null) != null ? [condition.value.host_header] : []
+        for_each = condition.value.host_header != null ? [condition.value.host_header] : []
         content {
           values = host_header.value.values
         }
       }
 
       dynamic "http_header" {
-        for_each = lookup(condition.value, "http_header", null) != null ? [condition.value.http_header] : []
+        for_each = condition.value.http_header != null ? [condition.value.http_header] : []
         content {
           http_header_name = http_header.value.http_header_name
           values           = http_header.value.values
@@ -184,22 +184,22 @@ resource "aws_lb_listener_rule" "listener_rule" {
       }
 
       dynamic "path_pattern" {
-        for_each = lookup(condition.value, "path_pattern", null) != null ? [condition.value.path_pattern] : []
+        for_each = condition.value.path_pattern != null ? [condition.value.path_pattern] : []
         content {
           values = path_pattern.value.values
         }
       }
 
       dynamic "query_string" {
-        for_each = lookup(condition.value, "query_string", null) != null ? condition.value.query_string : []
+        for_each = condition.value.query_string != null ? [condition.value.query_string] : []
         content {
-          key   = lookup(query_string.value, "key", null)
+          key   = query_string.value.key
           value = query_string.value.value
         }
       }
 
       dynamic "source_ip" {
-        for_each = lookup(condition.value, "source_ip", null) != null ? [condition.value.source_ip] : []
+        for_each = condition.value.source_ip != null ? [condition.value.source_ip] : []
         content {
           values = source_ip.value.values
         }
