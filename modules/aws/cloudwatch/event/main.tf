7410a114a6c8e33ccebf2cc4144fe4ffcabb003e
Zachary Hill <zhill@octo.ventures>
2025-02-06 14:56:49 -0500

    feat: add notifications to amplify and cloudwatch event

--- modules/aws/cloudwatch/event/main.tf
+++ modules/aws/cloudwatch/event/main.tf
@@ -8,14 +8,33 @@ terraform {
   }
 }
 
+###########################
+# CloudWatch EventBridge Rule
+###########################
 resource "aws_cloudwatch_event_rule" "event_rule" {
-  name                = var.name
   description         = var.description
+  event_bus_name      = var.event_bus_name
+  event_pattern       = var.event_pattern
+  name_prefix         = var.name_prefix
+  role_arn            = var.role_arn
   schedule_expression = var.schedule_expression
-  is_enabled          = var.is_enabled
+  state               = var.state
+  tags                = var.tags
 }
 
+###########################
+# CloudWatch EventBridge Target
+###########################
 resource "aws_cloudwatch_event_target" "event_target" {
-  rule = aws_cloudwatch_event_rule.event_rule.name
-  arn  = var.event_target_arn
+  arn       = var.event_target_arn
+  rule      = aws_cloudwatch_event_rule.event_rule.name
+  target_id = var.target_id
+
+  dynamic "input_transformer" {
+    for_each = var.input_transformer != null ? toset(var.input_transformer) : []
+    content {
+      input_paths    = input_transformer.value.input_paths
+      input_template = input_transformer.value.input_template
+    }
+  }
 }
