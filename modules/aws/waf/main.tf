e6dc899ddae2049b1a237b2d20dab68a1ece529a
jakeasaurus <jjones@slfcu.org>
2025-03-10 11:13:38 -0400

    initial waf

--- modules/aws/waf/main.tf
+++ modules/aws/waf/main.tf
@@ -8,8 +8,48 @@ terraform {
   }
 }
 
-resource "aws_vpn_connection_route" "vpn_route" {
-  #count                   = length(var.vpn_connection_id)
-  destination_cidr_block = var.vpn_route_cidr_block
-  vpn_connection_id      = var.vpn_connection_id
+############################
+# Data Sources
+############################
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+data "aws_organizations_organization" "current" {}
+
+############################
+# Locals
+############################
+
+
+
+
+
+############################
+# WAF ACL
+############################
+
+resource "aws_wafv2_web_acl" "waf_acl" {
+  name        = var.name
+  scope       = var.scope # REGIONAL(default) or CLOUDFRONT
+  description = var.description
+  default_action {
+    allow {}
+  }
+  rule {
+    name     = "allow-all"
+    priority = 1
+    action {
+      allow {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "allow-all"
+      sampled_requests_enabled   = true
+    }
+  }
 }
