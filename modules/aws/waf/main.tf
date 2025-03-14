482bf4c63c902a2050a4a9b184c26adc586b853d
jakeasaurus <jjones@slfcu.org>
2025-03-14 10:47:27 -0400

    did a little more work, added some vars, still missing some, added a todo

--- modules/aws/waf/main.tf
+++ modules/aws/waf/main.tf
@@ -31,25 +31,33 @@ resource "aws_wafv2_web_acl" "waf_acl" {
   name        = var.name
   scope       = var.scope # REGIONAL(default) or CLOUDFRONT
   description = var.description
-  default_action {
-    allow {}
+  dynamic "default_action" {
+    # TODO: start here for default action and add logic for allow/block for default.  
   }
-  rule {
-    name     = "allow-all"
-    priority = 1
-    action {
-      allow {}
-    }
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
+  dynamic "rule" {
+    for_each = each.value.rule != null ? each.value.rule : {}
+    content {
+      name     = rule.value.name
+      priority = rule.value.priority
+      action {
+        block {}
+      }
+      statement {
+        managed_rule_group_statement {
+          name        = rule.value.managed_rule_group_statement.name
+          vendor_name = rule.value.managed_rule_group_statement.vendor_name
+        }
+      }
+      visibility_config {
+        cloudwatch_metrics_enabled = rule.value.visibility_config.cloudwatch_metrics_enabled
+        metric_name                = rule.value.visibility_config.metric_name
+        sampled_requests_enabled   = rule.value.visibility_config.sampled_requests_enabled
       }
     }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "allow-all"
-      sampled_requests_enabled   = true
-    }
+  }
+  visibility_config {
+    cloudwatch_metrics_enabled = var.visibility_config.cloudwatch_metrics_enabled # default true
+    metric_name                = var.visibility_config.metric_name                # default "waf-acl" maybe change to var.name
+    sampled_requests_enabled   = var.visibility_config.sampled_requests_enabled   # default true
   }
 }
--- modules/aws/waf/variables.tf
+++ modules/aws/waf/variables.tf
@@ -1,1 +1,65 @@
+############################################
+# General Configuration
+############################################
 
+variable "name" {
+  type    = string
+  default = "default"
+}
+
+variable "scope" {
+  type    = string
+  default = "default"
+}
+
+variable "description" {
+  type    = string
+  default = "default"
+}
+
+############################################
+# Rule Configuration
+############################################
+
+variable "rule" {
+  description = "Map of rule configuration"
+  type = map(object({
+    name     = string
+    priority = number
+    action   = string
+    statement = object({
+      managed_rule_group_statement = object({
+        name           = string
+        vendor_name    = string
+        priority       = number
+        excluded_rules = list(string)
+      })
+    })
+    visibility_config = object({
+      cloudwatch_metrics_enabled = bool
+      metric_name                = string
+      sampled_requests_enabled   = bool
+    })
+  }))
+}
+
+
+
+############################################
+# Visibility Configuration
+############################################
+
+variable "visibility_config.cloudwatch_metrics_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "visibility_config.metric_name" {
+  type    = string
+  default = var.name
+}
+
+variable "visibility_config.sampled_requests_enabled" {
+  type    = bool
+  default = true
+}
