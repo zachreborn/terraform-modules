70abde6de3639263d72aa093581a5698262de45a
Zachary Hill <zhill@octo.ventures>
2025-03-21 14:48:10 -0400

    corrected outputs with key

--- modules/aws/api_gateway/outputs.tf
+++ modules/aws/api_gateway/outputs.tf
@@ -4,35 +4,35 @@
 
 output "id" {
   description = "The API identifier"
-  value       = aws_apigatewayv2_api.api.id
+  value       = aws_apigatewayv2_api.this.id
 }
 
 output "api_endpoint" {
   description = "The URI of the API"
-  value       = aws_apigatewayv2_api.api.api_endpoint
+  value       = aws_apigatewayv2_api.this.api_endpoint
 }
 
 output "arn" {
   description = "The ARN of the API"
-  value       = aws_apigatewayv2_api.api.arn
+  value       = aws_apigatewayv2_api.this.arn
 }
 
 output "execution_arn" {
   description = "The ARN prefix to be used in permission policies"
-  value       = aws_apigatewayv2_api.api.execution_arn
+  value       = aws_apigatewayv2_api.this.execution_arn
 }
 
 output "api_key_selection_expression" {
   description = "The API key selection expression for the API"
-  value       = aws_apigatewayv2_api.api.api_key_selection_expression
+  value       = aws_apigatewayv2_api.this.api_key_selection_expression
 }
 
 output "cors_configuration" {
   description = "The CORS configuration for the API"
-  value       = aws_apigatewayv2_api.api.cors_configuration
+  value       = aws_apigatewayv2_api.this.cors_configuration
 }
 
 output "tags_all" {
   description = "Map of tags assigned to the resource"
-  value       = aws_apigatewayv2_api.api.tags_all
+  value       = aws_apigatewayv2_api.this.tags_all
 }
