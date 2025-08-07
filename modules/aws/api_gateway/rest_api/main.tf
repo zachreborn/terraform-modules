44ed8b8989034647dad969420da780bcfe7d7c5b
jakeasaurus <jjones@slfcu.org>
2025-08-07 13:06:54 -0400

    updates... again

--- modules/aws/api_gateway/rest_api/main.tf
+++ modules/aws/api_gateway/rest_api/main.tf
@@ -153,8 +153,8 @@ resource "aws_s3_object" "truststore_pem" {
   count  = var.enable_mtls && var.domain_name != null ? 1 : 0
   bucket = aws_s3_bucket.mtls_truststore[0].id
   key    = "truststore/truststore.pem"
-  source = "${path.module}/../../../../truststore/truststore.pem"
-  etag   = filemd5("${path.module}/../../../../truststore/truststore.pem")
+  source = "${path.root}/truststore/truststore.pem"
+  etag   = filemd5("${path.root}/truststore/truststore.pem")
 
   tags = var.tags
 
