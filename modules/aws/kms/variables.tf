09d80ba4f334e866811c6ebc46cd8ef2f3aa2abc
Zachary Hill <zhill@octo.ventures>
2025-08-12 11:13:35 -0700

    docs: removed Defaults to... from var descriptions

--- modules/aws/kms/variables.tf
+++ modules/aws/kms/variables.tf
@@ -4,7 +4,7 @@
 
 variable "customer_master_key_spec" {
   type        = string
-  description = "(Optional) Specifies whether the key contains a symmetric key or an asymmetric key pair and the encryption algorithms or signing algorithms that the key supports. Valid values: SYMMETRIC_DEFAULT, RSA_2048, RSA_3072, RSA_4096, ECC_NIST_P256, ECC_NIST_P384, ECC_NIST_P521, or ECC_SECG_P256K1. Defaults to SYMMETRIC_DEFAULT. For help with choosing a key spec, see the AWS KMS Developer Guide."
+  description = "(Optional) Specifies whether the key contains a symmetric key or an asymmetric key pair and the encryption algorithms or signing algorithms that the key supports. Valid values: SYMMETRIC_DEFAULT, RSA_2048, RSA_3072, RSA_4096, ECC_NIST_P256, ECC_NIST_P384, ECC_NIST_P521, or ECC_SECG_P256K1. For help with choosing a key spec, see the AWS KMS Developer Guide."
   default     = "SYMMETRIC_DEFAULT"
   validation {
     condition     = can(regex("^(SYMMETRIC_DEFAULT|RSA_2048|RSA_3072|RSA_4096|ECC_NIST_P256|ECC_NIST_P384|ECC_NIST_P521|ECC_SECG_P256K1)$", var.customer_master_key_spec))
@@ -19,7 +19,7 @@ variable "description" {
 
 variable "deletion_window_in_days" {
   type        = number
-  description = "(Optional) Duration in days after which the key is deleted after destruction of the resource, must be between 7 and 30 days. Defaults to 30 days."
+  description = "(Optional) Duration in days after which the key is deleted after destruction of the resource, must be between 7 and 30 days."
   default     = 30
   validation {
     condition     = var.deletion_window_in_days >= 7 && var.deletion_window_in_days <= 30
@@ -29,7 +29,7 @@ variable "deletion_window_in_days" {
 
 variable "enable_key_rotation" {
   type        = bool
-  description = "(Optional) Specifies whether key rotation is enabled. Defaults to false."
+  description = "(Optional) Specifies whether key rotation is enabled."
   default     = true
 }
 
@@ -41,7 +41,7 @@ variable "key_usage" {
 
 variable "is_enabled" {
   type        = bool
-  description = "(Optional) Specifies whether the key is enabled. Defaults to true."
+  description = "(Optional) Specifies whether the key is enabled."
   default     = true
 }
 
