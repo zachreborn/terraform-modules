d14dc5ffbe6214dab66a00bb3d2d86888acc1991
Zachary Hill <zhill@octo.ventures>
2025-08-18 13:13:20 -0400

    converted to for_each

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -190,15 +190,16 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
-  count           = var.enable_s3_endpoint || var.enable_ecr_vpc_endpoints ? length(var.private_subnets_list) : 0
-  vpc_endpoint_id = aws_vpc_endpoint.s3[0]
-  route_table_id  = element(aws_route_table.private_route_table[*].id, count.index)
+  for_each        = var.enable_s3_endpoint || var.enable_ecr_vpc_endpoints ? toset(aws_subnet.private_subnets[*].id) : {}
+  route_table_id  = each.value
+  vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
+  # route_table_id  = element(aws_route_table.private_route_table[*].id, count.index)
 }
 
 resource "aws_vpc_endpoint_route_table_association" "public_s3" {
-  count           = var.enable_s3_endpoint || var.enable_ecr_vpc_endpoints ? length(var.public_subnets_list) : 0
-  vpc_endpoint_id = aws_vpc_endpoint.s3[0]
-  route_table_id  = aws_route_table.public_route_table[0].id
+  for_each        = var.enable_s3_endpoint || var.enable_ecr_vpc_endpoints ? toset(aws_subnet.public_subnets[*].id) : {}
+  vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
+  route_table_id  = each.value
 }
 
 ###########################
