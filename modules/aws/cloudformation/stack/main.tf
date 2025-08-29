b42c878c07e901577d2a6682a8cb6a0e64adc006
Zachary Hill <zhill@octo.ventures>
2025-08-29 17:19:51 -0400

    logic out of locals for readability

--- modules/aws/cloudformation/stack/main.tf
+++ modules/aws/cloudformation/stack/main.tf
@@ -20,14 +20,6 @@ terraform {
 # Locals
 ###########################
 
-locals {
-  disable_rollback = var.on_failure == null ? var.disable_rollback : false
-  on_failure       = var.disable_rollback ? null : var.on_failure
-  policy_body      = var.policy_url == null ? var.policy_body : null
-  policy_url       = var.policy_body == null ? var.policy_url : null
-  template_body    = var.template_url == null ? var.template_body : null
-  template_url     = var.template_body == null ? var.template_url : null
-}
 
 ###########################
 # Module Configuration
@@ -36,16 +28,16 @@ locals {
 
 resource "aws_cloudformation_stack" "this" {
   capabilities       = var.capabilities
-  disable_rollback   = local.disable_rollback
+  disable_rollback   = var.on_failure == null ? var.disable_rollback : false
   iam_role_arn       = var.iam_role_arn
   name               = var.name
   notification_arns  = var.notification_arns
-  on_failure         = local.on_failure
+  on_failure         = var.disable_rollback ? null : var.on_failure
   parameters         = var.parameters
-  policy_body        = local.policy_body
-  policy_url         = local.policy_url
+  policy_body        = var.policy_url == null ? var.policy_body : null
+  policy_url         = var.policy_body == null ? var.policy_url : null
   tags               = var.tags
-  template_body      = local.template_body
-  template_url       = local.template_url
+  template_body      = var.template_url == null ? var.template_body : null
+  template_url       = var.template_body == null ? var.template_url : null
   timeout_in_minutes = var.timeout_in_minutes
 }
