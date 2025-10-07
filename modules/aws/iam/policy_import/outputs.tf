1b48a278ce4ce35e352840b69d2ea6b35c1626eb
jakeasaurus <jjones@slfcu.org>
2025-10-07 14:18:42 -0400

    removed import versions and replaced with fixed originals

--- modules/aws/iam/policy_import/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "arn" {
-  value = aws_iam_policy.this.arn
-}
-
-output "id" {
-  value = aws_iam_policy.this.id
-}
--- modules/aws/iam/role_import/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "The Amazon Resource Name (ARN) specifying the role."
-  value       = aws_iam_role.this.arn
-}
-
-output "name" {
-  description = "The name of the role."
-  value       = aws_iam_role.this.name
-}
