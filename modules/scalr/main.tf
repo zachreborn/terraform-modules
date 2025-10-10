1dbec36feb9a2c50b6308e4ae8045559ed997cc1
Zachary Hill <zhill@octo.ventures>
2025-10-10 19:37:55 -0400

    feat: yaml config for environments and workspaces

--- modules/scalr/main.tf
+++ modules/scalr/main.tf
@@ -19,6 +19,37 @@ data "scalr_current_account" "account" {}
 ###########################
 # Locals
 ###########################
+locals {
+  yaml_config    = yamldecode(var.environments_config)
+  workspaces_map = { for environment, value in local.yaml_config : environment => value.workspaces }
+  workspaces = { for item in flatten([for environment, env_conf in yamldecode(file("example_config.yaml")) : [
+    for workspace_name, workspace_value in env_conf.workspaces : {
+      agent_pool_id               = workspace_value.agent_pool_id
+      auto_apply                  = workspace_value.auto_apply
+      auto_queue_runs             = workspace_value.auto_queue_runs
+      deletion_protection_enabled = workspace_value.deletion_protection_enabled
+      environment_id              = scalr_environment.this[environment].id
+      environment_name            = environment
+      execution_mode              = workspace_value.execution_mode
+      force_last_run              = workspace_value.force_last_run
+      hooks                       = workspace_value.hooks
+      iac_platform                = workspace_value.iac_platform
+      module_version_id           = workspace_value.module_version_id
+      name                        = workspace_value.name
+      operations                  = workspace_value.operations
+      provider_configuration      = workspace_value.provider_configuration
+      remote_backend              = workspace_value.remote_backend
+      remote_state_consumers      = workspace_value.remote_state_consumers
+      run_operation_timeout       = workspace_value.run_operation_timeout
+      ssh_key_id                  = workspace_value.ssh_key_id
+      tag_ids                     = workspace_value.tag_ids
+      terraform_version           = workspace_value.terraform_version
+      type                        = workspace_value.type
+      var_files                   = workspace_value.var_files
+      working_directory           = workspace_value.working_directory
+    }
+  ]]) : "${item.environment_name}.${item.workspace_name}" => item }
+}
 
 ###########################
 # Provider Configurations
@@ -38,7 +69,7 @@ resource "scalr_provider_configuration" "scalr" {
 resource "scalr_provider_configuration" "aws" {
   account_id             = data.scalr_current_account.account.id
   environments           = var.aws_environments
-  export_shell_variables = var.aws_export_shell_variables
+  export_shell_variables = var.export_shell_variables
   name                   = var.aws_provider_name
   owners                 = var.aws_owners
   aws {
@@ -53,20 +84,55 @@ resource "scalr_provider_configuration" "aws" {
   }
 }
 
+resource "scalr_provider_configuration_default" "this" {
+  environment_id            = var.default_environment_ids
+  provider_configuration_id = scalr_provider_configuration.aws.id
+}
+
 ###########################
 # Environment Configurations
 ###########################
 
 resource "scalr_environment" "this" {
-  for_each                        = var.environments
+  for_each                        = local.yaml_config
   account_id                      = data.scalr_current_account.account.id
   default_provider_configurations = each.value.default_provider_configurations
   default_workspace_agent_pool_id = each.value.default_workspace_agent_pool_id
   federated_environments          = each.value.federated_environments
-  mask_sensitive_output           = var.mask_sensitive_output
-  name                            = each.value.name
-  remote_backend                  = var.remote_backend
-  remote_backend_overridable      = var.remote_backend_overridable
+  mask_sensitive_output           = each.value.mask_sensitive_output
+  name                            = each.key
+  remote_backend                  = each.value.remote_backend
+  remote_backend_overridable      = each.value.remote_backend_overridable
   storage_profile_id              = each.value.storage_profile_id
-  tag_ids                         = var.tag_ids
+  tag_ids                         = each.value.tag_ids
+}
+
+###########################
+# Workspace Configurations
+###########################
+
+resource "scalr_workspace" "this" {
+  for_each                    = local.workspaces
+  agent_pool_id               = each.value.agent_pool_id != null ? each.value.agent_pool_id : var.default_workspace_agent_pool_id
+  auto_apply                  = each.value.auto_apply != null ? each.value.auto_apply : var.auto_apply
+  auto_queue_runs             = each.value.auto_queue_runs != null ? each.value.auto_queue_runs : var.auto_queue_runs
+  deletion_protection_enabled = each.value.deletion_protection_enabled != null ? each.value.deletion_protection_enabled : var.deletion_protection_enabled
+  environment_id              = each.value.environment_id
+  execution_mode              = each.value.execution_mode != null ? each.value.execution_mode : var.execution_mode
+  force_last_run              = each.value.force_last_run != null ? each.value.force_last_run : var.force_last_run
+  hooks                       = each.value.hooks
+  iac_platform                = each.value.iac_platform != null ? each.value.iac_platform : var.iac_platform
+  module_version_id           = each.value.module_version_id != null ? each.value.module_version_id : var.module_version_id
+  name                        = each.value.name
+  operations                  = each.value.operations != null ? each.value.operations : var.operations
+  provider_configuration      = each.value.provider_configuration
+  remote_backend              = each.value.remote_backend != null ? each.value.remote_backend : var.remote_backend
+  remote_state_consumers      = each.value.remote_state_consumers != null ? each.value.remote_state_consumers : var.remote_state_consumers
+  run_operation_timeout       = each.value.run_operation_timeout != null ? each.value.run_operation_timeout : var.run_operation_timeout
+  ssh_key_id                  = each.value.ssh_key_id != null ? each.value.ssh_key_id : var.ssh_key_id
+  tag_ids                     = each.value.tag_ids != null ? each.value.tag_ids : var.tag_ids
+  terraform_version           = each.value.terraform_version != null ? each.value.terraform_version : var.terraform_version
+  type                        = each.value.type != null ? each.value.type : var.type
+  var_files                   = each.value.var_files != null ? each.value.var_files : var.var_files
+  working_directory           = each.value.working_directory != null ? each.value.working_directory : var.working_directory
 }
--- modules/scalr/variables.tf
+++ modules/scalr/variables.tf
@@ -3,141 +3,272 @@
 ###########################
 
 variable "scalr_environments" {
-    description = "List of Scalr Environments which the provider will be shared to."
-    type        = list(string)
-    default     = null
+  description = "List of Scalr Environments which the provider will be shared to."
+  type        = list(string)
+  default     = null
 }
 
 variable "scalr_hostname" {
-    description = "The Scalr hostname."
-    type        = string
+  description = "The Scalr hostname."
+  type        = string
 }
 
 variable "scalr_provider_name" {
-    description = "Name of the Scalr Provider Configuration."
-    type        = string
-    default     = "scalr"
+  description = "Name of the Scalr Provider Configuration."
+  type        = string
+  default     = "scalr"
 }
 
 variable "scalr_owners" {
-    description = "List of Scalr User IDs who will own the Provider Configuration."
-    type        = list(string)
-    default     = []
+  description = "List of Scalr User IDs who will own the Provider Configuration."
+  type        = list(string)
+  default     = []
 }
 
 variable "scalr_token" {
-    description = "The Scalr API token."
-    type        = string
-    sensitive   = true
+  description = "The Scalr API token."
+  type        = string
+  sensitive   = true
 }
 
 ###########################
 # AWS Provider Variables
 ###########################
-
 variable "aws_access_key" {
-    description = "The AWS access key."
-    type        = string
-    sensitive   = true
-    default     = null
+  description = "The AWS access key."
+  type        = string
+  sensitive   = true
+  default     = null
 }
 
 variable "aws_account_type" {
-    description = "The type of AWS account. Valid values are 'regular', 'gov-cloud', and 'cn-cloud'."
-    type        = string
-    default     = "regular"
-    validation {
-        condition     = contains(["regular", "gov-cloud", "cn-cloud"], var.aws_account_type)
-        error_message = "The aws_account_type must be one of 'regular', 'gov-cloud', or 'cn-cloud'."
-    }
+  description = "The type of AWS account. Valid values are 'regular', 'gov-cloud', and 'cn-cloud'."
+  type        = string
+  default     = "regular"
+  validation {
+    condition     = contains(["regular", "gov-cloud", "cn-cloud"], var.aws_account_type)
+    error_message = "The aws_account_type must be one of 'regular', 'gov-cloud', or 'cn-cloud'."
+  }
 }
 
 variable "aws_audience" {
-    description = "The audience for the AWS credentials. Required if credentials_type is set to 'oidc'."
-    type        = string
-    default     = null
+  description = "The audience for the AWS credentials. Required if credentials_type is set to 'oidc'."
+  type        = string
+  default     = null
 }
 
 variable "aws_credentials_type" {
-    description = "The type of AWS credentials. Valid values are 'access_keys', 'oidc', and 'role_delegation'."
-    type        = string
-    default = "oidc"
-    validation {
-        condition     = contains(["access_keys", "oidc", "role_delegation"], var.aws_credentials_type)
-        error_message = "The aws_credentials_type must be one of 'access_keys', 'oidc', or 'role_delegation'."
-    }
+  description = "The type of AWS credentials. Valid values are 'access_keys', 'oidc', and 'role_delegation'."
+  type        = string
+  default     = "oidc"
+  validation {
+    condition     = contains(["access_keys", "oidc", "role_delegation"], var.aws_credentials_type)
+    error_message = "The aws_credentials_type must be one of 'access_keys', 'oidc', or 'role_delegation'."
+  }
 }
 
 variable "aws_environments" {
-    description = "List of Scalr Environments which the provider will be shared to."
-    type        = list(string)
-    default     = null
+  description = "List of Scalr Environments which the provider will be shared to."
+  type        = list(string)
+  default     = null
 }
 
 variable "aws_external_id" {
-    description = "The external ID to use when assuming the role. Required if aws_credentials_type is set to 'role_delegation' and the role requires an external ID."
-    type        = string
-    default     = null
-}
-
-variable "aws_export_shell_variables" {
-    description = "Whether to export AWS credentials as shell variables when using the Scalr CLI."
-    type        = bool
-    default     = false
+  description = "The external ID to use when assuming the role. Required if aws_credentials_type is set to 'role_delegation' and the role requires an external ID."
+  type        = string
+  default     = null
 }
 
 variable "aws_provider_name" {
-    description = "Name of the AWS Provider Configuration."
-    type        = string
-    default     = "aws"
+  description = "Name of the AWS Provider Configuration."
+  type        = string
+  default     = "aws"
 }
 
 variable "aws_owners" {
-    description = "List of Scalr User IDs who will own the Provider Configuration."
-    type        = list(string)
-    default     = null
+  description = "List of Scalr User IDs who will own the Provider Configuration."
+  type        = list(string)
+  default     = null
 }
 
 variable "aws_role_arn" {
-    description = "The ARN of the role to assume. Required if aws_credentials_type is set to 'oidc' or 'role_delegation'."
-    type        = string
-    default     = null
+  description = "The ARN of the role to assume. Required if aws_credentials_type is set to 'oidc' or 'role_delegation'."
+  type        = string
+  default     = null
 }
 
 variable "aws_secret_key" {
-    description = "The AWS secret key."
-    type        = string
-    sensitive   = true
-    default     = null
+  description = "The AWS secret key."
+  type        = string
+  sensitive   = true
+  default     = null
 }
 
 variable "aws_trusted_entity_type" {
-    description = "The type of trusted entity for the role. Valid values are 'aws_account' and 'aws_service'."
-    type        = string
-    default     = "aws_account"
-    validation {
-        condition     = contains(["aws_account", "aws_service"], var.aws_trusted_entity_type)
-        error_message = "The aws_trusted_entity_type must be one of 'aws_account' or 'aws_service'."
-    }
+  description = "The type of trusted entity for the role. Valid values are 'aws_account' and 'aws_service'."
+  type        = string
+  default     = "aws_account"
+  validation {
+    condition     = contains(["aws_account", "aws_service"], var.aws_trusted_entity_type)
+    error_message = "The aws_trusted_entity_type must be one of 'aws_account' or 'aws_service'."
+  }
+}
+
+###########################
+# Provider Default Variables
+###########################
+variable "default_environment_ids" {
+  description = "List of Environment IDs to set the default Provider Configurations in."
+  type        = list(string)
+  default     = null
 }
 
 ###########################
 # Environment Variables
 ###########################
-variable "environments" {
-    description = "Map of Environments."
-    type = map(object({
-        default_provider_configurations = optional(map(string)),
-        name                            = string,
-        remote_backend                  = optional(string),
-        remote_backend_overridable      = optional(bool),
-        storage_profile_id              = optional(string),
-        tag_ids                         = optional(list(string))
-    }))
+variable "default_workspace_agent_pool_id" {
+  description = "The default Agent Pool ID to assign to new Workspaces in the Environment."
+  type        = string
+  default     = null
 }
 
+###########################
+# Workspace Variables
+###########################
+variable "agent_pool_id" {
+  description = "The Agent Pool ID to assign to the Workspace. Can be overridden per workspace in the YAML file."
+  type        = string
+  default     = null
+}
+
+variable "auto_apply" {
+  description = "Whether to automatically apply runs when they are queued. Can be overridden per workspace in the YAML file."
+  type        = bool
+  default     = false
+}
+
+variable "auto_queue_runs" {
+  description = "Whether to automatically queue runs when a workspace's configuration changes. Can be overridden per workspace in the YAML file. Valid values are 'skip_first', 'always', 'never', and 'on_create_only'."
+  type        = string
+  default     = "always"
+  validation {
+    condition     = contains(["skip_first", "always", "never", "on_create_only"], var.auto_queue_runs)
+    error_message = "The auto_queue_runs must be one of 'skip_first', 'always', 'never', or 'on_create_only'."
+  }
+}
+
+variable "deletion_protection_enabled" {
+  description = "Whether to enable deletion protection for the workspace. Can be overridden per workspace in the YAML file."
+  type        = bool
+  default     = true
+}
+
+variable "execution_mode" {
+  description = "The execution mode for the workspace. Can be overridden per workspace in the YAML file. Valid values are 'remote' and 'local'."
+  type        = string
+  default     = "remote"
+  validation {
+    condition     = contains(["remote", "local"], var.execution_mode)
+    error_message = "The execution_mode must be one of 'remote' or 'local'."
+  }
+}
+
+variable "force_latest_run" {
+  description = "Whether to force a new run to be created for the workspace. Can be overridden per workspace in the YAML file."
+  type        = bool
+  default     = false
+}
+
+variable "iac_platform" {
+  description = "The Infrastructure as Code platform for the workspace. Valid values are 'terraform' or 'opentofu'."
+  type        = string
+  default     = "opentofu"
+  validation {
+    condition     = contains(["terraform", "opentofu"], var.iac_platform)
+    error_message = "The iac_platform must be one of 'terraform' or 'opentofu'."
+  }
+}
+
+variable "module_version_id" {
+  description = "The Module Version ID to use for the workspace. Can be overridden per workspace in the YAML file. Must be in the format 'modver-<RANDOM STRING>'. This cannot be set when using a vcs repository as the source for the workspace."
+  type        = string
+  default     = null
+}
+
+variable "operations" {
+  description = "Whether to enable remote execution for the workspace. When set to false, the workspace only stores its state. Can be overridden per workspace in the YAML file."
+  type        = bool
+  default     = true
+}
+
+variable "remote_backend" {
+  description = "Whether Scalr manages the remote backend configuration. Can be overridden per workspace in the YAML file."
+  type        = bool
+  default     = true
+}
+
+variable "remote_state_consumers" {
+  description = "List of Workspace IDs that can read the remote state of this workspace. Can be overridden per workspace in the YAML file."
+  type        = list(string)
+  default     = null
+}
+
+variable "run_operation_timeout" {
+  description = "The maximum time, in minutes, that a run operation (plan or apply) can take before it is automatically canceled. Can be overridden per workspace in the YAML file."
+  type        = number
+  default     = 60
+}
+
+variable "ssh_key_id" {
+  description = "The SSH Key ID to use for the workspace. Can be overridden per workspace in the YAML file."
+  type        = string
+  default     = null
+}
+
+variable "tag_ids" {
+  description = "List of Tag IDs to assign to the workspace. Can be overridden per workspace in the YAML file."
+  type        = list(string)
+  default     = null
+}
+
+variable "terraform_version" {
+  description = "The opentofu or terraform version to use for the workspace. Can be overridden per workspace in the YAML file. Must be in the format 'X.Y.Z'."
+  type        = string
+  default     = null
+}
+
+variable "type" {
+  description = "The type of workspace. Valid values are 'production', 'staging', 'testing', 'development', and 'unmapped'."
+  type        = string
+  default     = "production"
+  validation {
+    condition     = contains(["production", "staging", "testing", "development", "unmapped"], var.type)
+    error_message = "The type must be one of 'production', 'staging', 'testing', 'development', or 'unmapped'."
+  }
+}
+
+variable "var_files" {
+  description = "A list of paths which hold the '.tfvars' files for the workspace. Can be overridden per workspace in the YAML file."
+  type        = list(string)
+  default     = []
+}
+
+variable "working_directory" {
+  description = "The working directory as a relative path which opentofu or terraform will run for the workspace. Can be overridden per workspace in the YAML file."
+  type        = string
+  default     = null
+}
 
 ###########################
 # General Variables
 ###########################
+variable "environments_config" {
+  description = "YAML formatted file defining environments and their workspaces."
+  type        = string
+}
 
+variable "export_shell_variables" {
+  description = "Whether to export provider credentials as shell variables when using the Scalr CLI."
+  type        = bool
+  default     = false
+}
