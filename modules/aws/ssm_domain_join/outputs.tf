20b86c84a09159241cb0a7360bc7d903e0f00424
bradms98 <bradms98@gmail.com>
2026-05-14 11:17:04 -0600

    fix: address zach review feedback on ssm_domain_join module

--- modules/aws/ssm_domain_join/outputs.tf
+++ modules/aws/ssm_domain_join/outputs.tf
@@ -1,14 +1,17 @@
-output "ssm_document_name" {
-  value       = aws_ssm_document.default.name
-  description = "The name of the SSM domain join document."
+########################################
+# SSM Domain Join Outputs
+########################################
+output "ssm_document_arn" {
+  description = "ARN of the SSM domain join document."
+  value       = aws_ssm_document.this.arn
 }
 
-output "ssm_document_arn" {
-  value       = aws_ssm_document.default.arn
-  description = "The ARN of the SSM domain join document."
+output "ssm_document_name" {
+  description = "Name of the SSM domain join document."
+  value       = aws_ssm_document.this.name
 }
 
 output "ssm_association_id" {
-  value       = aws_ssm_association.default.association_id
-  description = "The ID of the SSM association."
+  description = "ID of the SSM State Manager association."
+  value       = aws_ssm_association.this.association_id
 }
